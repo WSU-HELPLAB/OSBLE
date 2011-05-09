074c7f78986ce20df3cb12f00efa8f6945063b52
jneufeld_cp <SND\jneufeld_cp>
2011-05-09 22:17:29 +0000

    Working on course/user connections. Start of notifications.

---
+++ trunk/web/OSBLE/Models/Notifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OSBLE.Models
+{
+    public class Notifications
+    {
+        [Required]
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int UserProfileID { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+        public bool Read { get; set; }
+
+        public DateTime Posted { get; set; }
+
+        public string TableName { get; set; }
+
+        public int TableID { get; set; }
+    }
+}
