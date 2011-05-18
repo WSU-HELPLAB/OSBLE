732f698f3429768a05e1fab3a4e69ef2765be823
jneufeld_cp <SND\jneufeld_cp>
2011-05-18 16:30:52 +0000

    - Rolled back previous route changes - Moved dashboard mode radio boxes into activity feed area

--- trunk/web/OSBLE/Global.asax.cs
+++ trunk/web/OSBLE/Global.asax.cs
@@ -22,6 +22,8 @@ namespace OSBLE
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
+
+
         }
 
         protected void Application_Start()
