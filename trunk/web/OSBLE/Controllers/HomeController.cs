626a90dcd420b8e30d2e7f2010a6c38563220567
nomis2005_cp <SND\nomis2005_cp>
2011-05-19 20:56:57 +0000

    Dashboard posts now reconize and support links

--- trunk/web/OSBLE/Controllers/HomeController.cs
+++ trunk/web/OSBLE/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using OSBLE.Attributes;
 using OSBLE.Models;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Caching;
-using System.Web.UI;
-using System.Web;
 
 namespace OSBLE.Controllers
 {
@@ -17,7 +15,7 @@ namespace OSBLE.Controllers
         /// Main action for the OSBLE Dashboard
         /// </summary>
         /// <returns></returns>
-        /// 
+        ///
         [RequireActiveCourse]
         public ActionResult Index()
         {
@@ -32,8 +30,20 @@ namespace OSBLE.Controllers
             bool DashboardSingleCourseMode = ViewBag.DashboardSingleCourseMode = context.Session["DashboardSingleCourseMode"];
 
             #region Activity Feed Posting
+
             // Feed Post attempt
-            if (Request.Form["post_message"] != null)
+
+            string content = null;
+            try
+            {
+                content = Request.Form["post_content"];
+            }
+            catch (HttpRequestValidationException e)
+            {
+                content = null;
+            }
+
+            if (content != null)
             {
                 // TODO: Allow courses to allow students to post to first level of feed.
                 int replyTo = 0;
@@ -41,34 +51,41 @@ namespace OSBLE.Controllers
                 {
                     replyTo = Convert.ToInt32(Request.Form["reply_to"]);
                 }
-                
+
                 DashboardPost dp = new DashboardPost();
-                dp.Content = Request.Form["post_content"];
+                dp.Content = content;
                 dp.UserProfile = currentUser;
 
-                if(replyTo != 0) {
-                    DashboardPost replyToPost = db.DashboardPosts.Find(replyTo); 
-                    if(replyToPost != null) { // Does the post we're replying to exist?
+                if (replyTo != 0)
+                {
+                    DashboardPost replyToPost = db.DashboardPosts.Find(replyTo);
+                    if (replyToPost != null)
+                    { // Does the post we're replying to exist?
                         // Are we a member of the course we're replying to?
                         CoursesUsers cu = (from c in currentCourses
-                                          where c.CourseID == replyToPost.CourseID
-                                          select c).FirstOrDefault();
+                                           where c.CourseID == replyToPost.CourseID
+                                           select c).FirstOrDefault();
 
                         DashboardReply dr = new DashboardReply();
                         dr.Content = dp.Content;
                         dr.UserProfile = dp.UserProfile;
-                        
-                        if(cu != null) {
+
+                        if (cu != null)
+                        {
                             replyToPost.Replies.Add(dr);
-                        }                               
-                                                    
+                        }
                     }
-                } else {
+                }
+                else
+                {
                     List<CoursesUsers> CoursesToPost = new List<CoursesUsers>();
 
-                    if(Request.Form["post_active"] != null) { // Post to active course only.
+                    if (Request.Form["post_active"] != null)
+                    { // Post to active course only.
                         CoursesToPost.Add(activeCourse);
-                    } else if(Request.Form["post_all"] != null) { // Post to all courses.
+                    }
+                    else if (Request.Form["post_all"] != null)
+                    { // Post to all courses.
                         CoursesToPost = currentCourses;
                     }
 
@@ -91,16 +108,20 @@ namespace OSBLE.Controllers
                 return RedirectToAction("Index"); // Redirect so we don't have refresh data
             }
 
-            #endregion
+            #endregion Activity Feed Posting
 
             #region Activity Feed View
 
             List<int> ViewedCourses = new List<int>();
 
-            if(DashboardSingleCourseMode) {
+            if (DashboardSingleCourseMode)
+            {
                 ViewedCourses.Add(activeCourse.CourseID);
-            } else {
-                foreach(CoursesUsers cu in currentCourses) {
+            }
+            else
+            {
+                foreach (CoursesUsers cu in currentCourses)
+                {
                     ViewedCourses.Add(cu.CourseID);
                 }
             }
@@ -108,7 +129,7 @@ namespace OSBLE.Controllers
             // TODO: Add Pagination
             ViewBag.DashboardPosts = db.DashboardPosts.Where(d => ViewedCourses.Contains(d.CourseID)).OrderByDescending(d => d.Posted).ToList();
 
-            #endregion
+            #endregion Activity Feed View
 
             return View();
         }
@@ -117,12 +138,12 @@ namespace OSBLE.Controllers
         {
             if (ActiveCourse != null)
             {
-                return RedirectToAction("Index");           
+                return RedirectToAction("Index");
             }
 
             ViewBag.CurrentTab = "Dashboard";
 
-            return View(); 
+            return View();
         }
 
         public ActionResult NoAccess()
@@ -177,7 +198,6 @@ namespace OSBLE.Controllers
                     // Non-integer input. Default to false.
                     context.Session["DashboardSingleCourseMode"] = false;
                 }
-
             }
 
             // Return to Dashboard.
@@ -259,6 +279,5 @@ namespace OSBLE.Controllers
                 return new FileStreamResult(FileSystem.GetDefaultProfilePicture(), "image/jpeg");
             }
         }
-
     }
 }
