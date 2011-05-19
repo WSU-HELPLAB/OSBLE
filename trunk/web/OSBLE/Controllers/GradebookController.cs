beac7b29e915864529eeff784986d07e09babf82
aphanson17_cp <SND\aphanson17_cp>
2011-05-19 17:52:51 +0000

    I have added a gradebook controller and view. The view is the start of the spreadsheet for the gradebook.

---
+++ trunk/web/OSBLE/Controllers/GradebookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSBLE.Controllers
+{
+    public class GradebookController : Controller
+    {
+        //
+        // GET: /Gradebook/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
