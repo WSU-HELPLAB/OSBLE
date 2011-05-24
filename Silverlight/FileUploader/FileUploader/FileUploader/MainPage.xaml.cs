a9f4e391522670e3c77093b96a99adeccea5afb6
kdahl15_cp <SND\kdahl15_cp>
2011-05-24 19:40:30 +0000

    Added fuctionality to the move up and move down btn's and cleaned up the select event.  (made it more efficient)

--- Silverlight/FileUploader/FileUploader/FileUploader/MainPage.xaml.cs
+++ Silverlight/FileUploader/FileUploader/FileUploader/MainPage.xaml.cs
@@ -11,15 +11,20 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Windows.Media.Imaging;
+using FileUploader.OsbleServices;
 
 namespace FileUploader
 {
     
     public partial class MainPage : UserControl
     {
+        // Local Directory path
        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
        string home = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+        // Synced Directory path
+
+        // Double Click variables
        string lastTarget = "";
        bool wasClicked = false;
  
@@ -27,19 +32,22 @@ namespace FileUploader
         {
             InitializeComponent();
          
+            // Populate the ListBox's LocalFileList and SyncedFileList with the contents of the directory path
             dirList(path, LocalFileList);
-            dirList(path, RemoteFileList);
+            dirList(path, SyncedFileList);
         
         }
 
         private void dirList(string path, ListBox filelist)
         {
             filelist.Items.Clear();
+            //If you have entered a subdirectory of home (Back button so to speak)
             if (path != home)
             {
-                filelist.Items.Add("...");
+                filelist.Items.Add("..");
             }
 
+            // Assigns all the directories to the ListBox
             foreach (string folder in Directory.EnumerateDirectories(path))
             {
                 DirectoryLabel label = new DirectoryLabel();
@@ -47,7 +55,8 @@ namespace FileUploader
                 label.Image = LabelImage.Folder;
                 filelist.Items.Add(label);
             }
-
+             
+            // Assigns all the files to the ListBox
             foreach (string file in Directory.EnumerateFiles(path))
             {
                 DirectoryLabel label = new DirectoryLabel();
@@ -59,67 +68,123 @@ namespace FileUploader
             InitializeComponent();
         }
 
+
         private void LocalDirSelect(object sender, MouseButtonEventArgs e)
         {
-            DirectoryLabel selected = this.LocalFileList.SelectedItem as DirectoryLabel;
-            if (selected != null && selected.Image != LabelImage.File)
+            //If selected is a file or directory
+            if ( this.LocalFileList.SelectedItem is DirectoryLabel)
             {
-                if (wasClicked == true)
+                DirectoryLabel selected = this.LocalFileList.SelectedItem as DirectoryLabel;
+                // if selected is a directory
+                if (selected.Image != LabelImage.File)
                 {
-                    if (selected.DirName.Text == lastTarget)
+                    if (wasClicked == true)
                     {
-                        path += "\\" + selected.DirName.Text;
-                        dirList(path, LocalFileList);
+                        if (selected.DirName.Text == lastTarget)
+                        {
+                            // appends the selected directory to the current path to open directory
+                            path += "\\" + selected.DirName.Text;
+                            dirList(path, LocalFileList);
+                        }
                     }
-                }
-                else
-                {
-                    wasClicked = true;
+                    else
+                    {
+                        wasClicked = true;
+                    }
                 }
                 lastTarget = selected.DirName.Text;
 
+                // If it was a directory return out
+                return;
             }
-            else if (this.LocalFileList.SelectedItem.ToString() == "...")
+            else if (this.LocalFileList.SelectedItem.ToString() == "..")
             {
                 Stack<string> path_pieces = new Stack<string>(path.Split('\\'));
                 path_pieces.Pop();
                 path = String.Join("\\", path_pieces.ToArray().Reverse());
                 dirList(path, LocalFileList);
-                lastTarget = "";
-                wasClicked = false;
             }
-            else
-            {
-                lastTarget = "";
-                wasClicked = false;
-            }
-
-            
           
-            
+            // If it wasn't a directory reset the click variables
+            lastTarget = "";
+            wasClicked = false;
         }
 
-        private void RemoteDirSelect(object sender, MouseButtonEventArgs e)
+        private void SyncDirSelect(object sender, MouseButtonEventArgs e)
         {
-            DirectoryLabel selected = this.RemoteFileList.SelectedItem as DirectoryLabel;
-            if (selected != null && selected.Image != LabelImage.File)
+            if(this.SyncedFileList.SelectedItem is DirectoryLabel)
             {
-                path += "\\" + selected.DirName.Text;
-                dirList(path, RemoteFileList);
+                DirectoryLabel selected = this.SyncedFileList.SelectedItem as DirectoryLabel;
+                if (selected.Image != LabelImage.File)
+                {
+                    if (wasClicked == true)
+                    {
+                        if (selected.DirName.Text == lastTarget)
+                        {
+                            path += "\\" + selected.DirName.Text;
+                            //dirList(path, SyncedFileList);
+                        }
+                    }
+                    else
+                    {
+                        wasClicked = true;
+                    }
+                }
+                lastTarget = selected.DirName.Text;
+
+                // If it was a directory return out
+                return;
+                
             }
-            else if (this.RemoteFileList.SelectedItem.ToString() == "...")
+            else if (this.SyncedFileList.SelectedItem.ToString() == "..")
             {
                 Stack<string> path_pieces = new Stack<string>(path.Split('\\'));
                 path_pieces.Pop();
                 path = String.Join("\\", path_pieces.ToArray().Reverse());
-                dirList(path, RemoteFileList);
+                //dirList(path, SyncedFileList);
             }
 
+            // If it wasn't a directory reset the click variables
+            lastTarget = "";
+            wasClicked = false;
         }
 
         private void Syncbtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void MoveUpbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.SyncedFileList.SelectedItem is ListBoxItem)
+            {
+                int index = this.SyncedFileList.SelectedIndex;
+                object Swap = this.SyncedFileList.SelectedItem;
+                if (index > 0)
+                {
+                    this.SyncedFileList.Items.RemoveAt(index);
+                    this.SyncedFileList.Items.Insert(index - 1, Swap);
+                    this.SyncedFileList.SelectedItem = Swap;
+                }
+            }
+        }
+
+        private void MoveDownbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.SyncedFileList.SelectedItem is ListBoxItem)
+            {
+                if (this.SyncedFileList.SelectedItem is ListBoxItem)
+                {
+                    int index = this.SyncedFileList.SelectedIndex;
+                    object Swap = this.SyncedFileList.SelectedItem;
+                    if (index != -1 && index < this.SyncedFileList.Items.Count - 1)
+                    {
+                        this.SyncedFileList.Items.RemoveAt(index);
+                        this.SyncedFileList.Items.Insert(index + 1, Swap);
+                        this.SyncedFileList.SelectedItem = Swap;
+                    }
+                }
+            }
         }
     }
 }
