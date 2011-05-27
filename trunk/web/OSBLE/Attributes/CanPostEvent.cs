ef8d37c664a28a152953afccdfd18dc3e37e8297
jneufeld_cp <SND\jneufeld_cp>
2011-05-27 17:40:12 +0000

    New action filter, removed fluent date (reference not working at the moment)

---
+++ trunk/web/OSBLE/Attributes/CanPostEvent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using OSBLE.Controllers;
+using OSBLE.Models;
+
+namespace OSBLE.Attributes
+{
+    /// <summary>
+    /// Redirects to index if user has no modify permissions for current course.
+    /// </summary>
+    /// 
+    public class CanPostEvent : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller is OSBLEController)
+            {
+                OSBLEController controller = filterContext.Controller as OSBLEController;
+
+                AbstractCourse ac = controller.ActiveCourse.Course;
+
+                if (ac is Course)
+                {
+                    // Course allows Instructors or TAs to post events.
+                    if (!(controller.ActiveCourse.CourseRole.CanGrade || controller.ActiveCourse.Course.AllowEventPosting))
+                    {
+                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+                    }
+
+                }
+                else
+                { // Community
+
+                    // Community allows only Leaders to post events.
+                    if (!(controller.ActiveCourse.CourseRole.CanModify || controller.ActiveCourse.Course.AllowEventPosting))
+                    {
+                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ trunk/web/OSBLE/Models/Event.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OSBLE.Models
+{
+    public class Event
+    {
+        [Required]
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int CourseID { get; set; }
+        public virtual AbstractCourse Course { get; set; }
+
+        [Required]
+        public DateTime Posted { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Link { get; set; }
+
+        public bool Approved { get; set; }
+
+        public Event()
+            : base()
+        {
+            Posted = DateTime.Now;
+        }
+    }
+}
