a7a6b4148e3c1ffb57212942542238051edb8d11
mario87_cp <SND\mario87_cp>
2011-06-06 22:56:40 +0000

    -Changed CustomDataGrid.cs to allow the bottom row of a grid to only be two cells
    -RubricModel.cs:
    -Added two last columns for add column button & comment column
    -Added last row for add row button
    -Fixed delete row/column
    -Fixed some bugs
    
    Still a lot of bugs created in unique instances. More work needed

--- Silverlight/OsbleRubric/OsbleRubric/RubricModel.cs
+++ Silverlight/OsbleRubric/OsbleRubric/RubricModel.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ChemProV.PFD.Streams.PropertiesWindow;
+using System.Collections.Generic;
 
 namespace OsbleRubric
 {
@@ -25,8 +26,15 @@ namespace OsbleRubric
         const double RowHeight = 138;
         const double Col1Width = 150;
         const int GlobalFontSize = 12;
+        const double finalRowHeight = 32;
+
+        const double addColColumnWidth = 32;
+        const double commentColumnWidth = 150;
+
+
         private ImageSource helpIconSource = new BitmapImage(new Uri("Icons/help.png", UriKind.Relative));
         private ImageSource deleteIconSource = new BitmapImage(new Uri("Icons/delete.png", UriKind.Relative));
+        private ImageSource addIconSource = new BitmapImage(new Uri("Icons/add.png", UriKind.Relative));
 
         #endregion constants
 
@@ -46,6 +54,7 @@ namespace OsbleRubric
             //setting borders for grid
             customDataGrid.HideBordersForLastRow = false;
             customDataGrid.HideBordersForLastTwoColumns = false;
+            customDataGrid.LastRowAsTwoCells = true;
             customDataGrid.BorderBrush = new SolidColorBrush(Colors.Black);
             customDataGrid.BorderThickness = 1.0;
 
@@ -53,99 +62,199 @@ namespace OsbleRubric
             customDataGrid.PlaceUIElement(createCol0Row0(), 0, 0);
             customDataGrid.PlaceUIElement(createCol1Row0(), 1, 0);
             customDataGrid.PlaceUIElement(createLevelTitleCell(), 2, 0);
+            //
+            createAddButtonColumn();
+            createCommentColumn();
             createCriterionRow();
+            createAddButtonRow();
+            
 
             //setting widths/heights for the table - possibly best to set width/height somewhere else
+            //setting heights for rows
             customDataGrid.BaseGrid.RowDefinitions[0].Height = new GridLength(Row0Height);
-            for (int i = 1; i < customDataGrid.BaseGrid.RowDefinitions.Count; ++i)
+            for (int i = 1; i < customDataGrid.BaseGrid.RowDefinitions.Count-1; ++i)
             {
                 customDataGrid.BaseGrid.RowDefinitions[i].Height = new GridLength(RowHeight);
             }
+            //customDataGrid.BaseGrid.RowDefinitions[customDataGrid.BaseGrid.RowDefinitions.Count - 1].Height = new GridLength(finalRowHeight);
+
+            //setting widths for columns
             customDataGrid.BaseGrid.ColumnDefinitions[0].Width = new GridLength(Col0Width);
             customDataGrid.BaseGrid.ColumnDefinitions[1].Width = new GridLength(Col1Width);
-            for (int i = 2; i < customDataGrid.BaseGrid.ColumnDefinitions.Count; ++i)
+            for (int i = 2; i < customDataGrid.BaseGrid.ColumnDefinitions.Count - 2; ++i)
             {
                 customDataGrid.BaseGrid.ColumnDefinitions[i].Width = new GridLength(ColWidth);
             }
         }
 
-        private void removeRow()
+
+        //This method creates the comment column and adds it to the grid (last column)
+        private void createCommentColumn()
         {
-            customDataGrid.RemoveRow(2);
-            /*
-            int toRemove = 2;
-            //for this, look at the row deleting, all rows before that (lower index) dont change, all rows after, reduce index by 1 (to account for the delete) and then delete the last row (might not be last, probably second to last because of bottom row)
-            //problems might be hooking up to DB
-
-            //new strat: delete all UIielements in each column, then use removeat
-
-            /*
-            for(int i = 0; i < myGrid.BaseGrid.ColumnDefinitions.Count; ++i)
+            StackPanel stackPanel = new StackPanel()
             {
-            }
-            List<FrameworkElement> elementsToRemove = new List<FrameworkElement>();
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(5, 5, 5, 5),
+                Orientation = Orientation.Horizontal,
+                
+            };
 
-            foreach (FrameworkElement element in myGrid.BaseGrid.Children)
+            CheckBox commentsOn = new CheckBox()
             {
-                if (Grid.GetRow(element) == toRemove)
-                {
-                    elementsToRemove.Add(element);
-                }
-            }
-            foreach (FrameworkElement element in elementsToRemove)
+                IsChecked = true,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            TextBlock commentsTB = new TextBlock()
             {
-                // myGrid.BaseGrid.Children.Remove(element);
-            }
+                Text = "Comments",
+                FontWeight = FontWeights.Bold,
+                FontSize = GlobalFontSize,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            Image helpIcon = new Image()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Source = helpIconSource,
+                Height = 16,
+                Width = 16,
+            };
+            ToolTipService.SetToolTip(helpIcon, "This checkbox determines if a column is included in the final table");
 
-            //myGrid.BaseGrid.RowDefinitions.RemoveAt(toRemove-1);
-             * */
+            stackPanel.Children.Add(commentsOn);
+            stackPanel.Children.Add(commentsTB);
+            stackPanel.Children.Add(helpIcon);
+
+            int columnToPlace = customDataGrid.BaseGrid.ColumnDefinitions.Count;
+            customDataGrid.PlaceUIElement(stackPanel, columnToPlace, 0);
+
+            for (int i = 1; i < customDataGrid.BaseGrid.RowDefinitions.Count - 1; ++i)
+            {
+                customDataGrid.PlaceUIElement(createCommentCell(), columnToPlace, i);
+            }
         }
 
-        private void removeCol()
+        //This method creates the UI elements to be inserted into a comment cell
+        private TextBox createCommentCell()
         {
-            customDataGrid.RemoveColumn(2);
-            /*
-            //do combo of this plus remove the last. then change all row definitions. ugh.
-            int toRemove = 2;
-
-            //removes UI elements
-
-            for (int i = myGrid.BaseGrid.RowDefinitions.Count-1 ; i >=0 ; --i)
+            TextBox commentBox = new TextBox()
             {
-                myGrid.RemoveUIElementAt(toRemove, i);
-            }
-
-            //myGrid.UpdateGridSize();
-            //Tries to remove the column...not working correctly
-           // myGrid.BaseGrid.ColumnDefinitions.RemoveAt(toRemove);
-            */
+                Width = commentColumnWidth,
+                Margin = new Thickness(5, 5, 5, 5),
+                IsReadOnly = true,
+            };
+            return commentBox;
         }
 
-        //This method creates and correctly populates an entire column
+        //This method creates the add button column and adds it to the grid (second to last column)
+        private void createAddButtonColumn()
+        {
+            //stack Panel to be added
+            StackPanel stackPanel = new StackPanel()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = addColColumnWidth,
+                Margin = new Thickness(5, 5, 5, 5)
+            };
+            //image to be added to stackPanel
+            Image addIcon = new Image()
+            {
+                Source = addIconSource,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Height = 32,
+                Width = 32
+            };
+
+            //adding the image and setting up its event
+            stackPanel.Children.Add(addIcon);
+            addIcon.MouseLeftButtonDown += new MouseButtonEventHandler(addCol_MouseLeftButtonDown);
+
+            //adding column into grid
+            customDataGrid.PlaceUIElement(stackPanel, customDataGrid.BaseGrid.ColumnDefinitions.Count , 0);
+        }
+
+        //This method creates the add button row and adds it to the bottom of the grid
+        private void createAddButtonRow()
+        {
+            //stack Panel to be added
+            StackPanel stackPanel = new StackPanel() 
+            { 
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = ColWidth,
+                Height = finalRowHeight,
+                Margin = new Thickness(5, 5, 5, 5)
+            };
+            //image to be added to stackPanel
+            Image addIcon = new Image() 
+            { 
+                Source = addIconSource, 
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Height = 32,
+                Width = 32 
+            };
+
+            //adding the image and setting up its event
+            stackPanel.Children.Add(addIcon);
+            addIcon.MouseLeftButtonDown += new MouseButtonEventHandler(addRow_MouseLeftButtonDown);
+
+            //textblock for bottom row
+            TextBlock pointsPossible_TextBlock = new TextBlock()
+            {
+                Text = "Points Possible: ",
+                FontSize = GlobalFontSize,
+                FontWeight = FontWeights.Bold,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom
+            };
+
+            //setting the textblock to span all remaining columns
+            pointsPossible_TextBlock.SetValue(Grid.ColumnSpanProperty, customDataGrid.BaseGrid.ColumnDefinitions.Count - 1);
+
+            //adding UIelements into the grid
+            int rowToAdd = customDataGrid.BaseGrid.RowDefinitions.Count;
+            customDataGrid.PlaceUIElement(stackPanel, 0, rowToAdd);
+            customDataGrid.PlaceUIElement(pointsPossible_TextBlock, 1, rowToAdd);
+        }
+
+        //This method creates and correctly populates an entire column, and then adds it to the grid
         private void createLevelColumn()
         {
-            //create a column, must go from 0 to max-1 (handle the latter later)
-            //in 0 put createLevelTitleCell, in rest, they need createLevelCell()
             int colIndexToInsert = customDataGrid.BaseGrid.ColumnDefinitions.Count;
+
+            //adding the top row cell
             customDataGrid.PlaceUIElement(createLevelTitleCell(), colIndexToInsert, 0);
-            for (int i = 1; i < customDataGrid.BaseGrid.RowDefinitions.Count; i++)
+
+            //adding the rest of the cells
+            for (int i = 1; i < customDataGrid.BaseGrid.RowDefinitions.Count-1; i++)
             {
                 customDataGrid.PlaceUIElement(createLevelCell(), colIndexToInsert, i);
             }
         }
 
-        //This method creates and correctly populates an entire row
+        //This method creates and correctly populates an entire row, and then adds it to the grid
         private void createCriterionRow()
         {
             int rowPlacement = customDataGrid.BaseGrid.RowDefinitions.Count;
+
+            //adding first two column cells
             customDataGrid.PlaceUIElement(createPerformanceCritCell(), 0, rowPlacement);
             customDataGrid.PlaceUIElement(createCritWeightCell(), 1, rowPlacement);
 
-            for (int i = 2; i < customDataGrid.BaseGrid.ColumnDefinitions.Count; i++)
+            //adding the rest of the cells 
+            for (int i = 2; i < customDataGrid.BaseGrid.ColumnDefinitions.Count-2; i++)
             {
                 customDataGrid.PlaceUIElement(createLevelCell(), i, rowPlacement);
             }
+
+            //adding comment cell
+            customDataGrid.PlaceUIElement(createCommentCell(), customDataGrid.BaseGrid.ColumnDefinitions.Count - 1, rowPlacement);
         }
+
+
 
         //This method creates the cells used for a Level column (With the exception of the first row)
         private StackPanel createLevelCell()
@@ -206,7 +315,7 @@ namespace OsbleRubric
                 TextWrapping = TextWrapping.Wrap
             };
             Image deleteIcon = new Image { Source = deleteIconSource, Height = 32, Width = 32, Margin = new Thickness(0, 3, 0, 0) };
-            deleteIcon.MouseLeftButtonDown += new MouseButtonEventHandler(Delete_MouseLeftButtonDown);
+            deleteIcon.MouseLeftButtonDown += new MouseButtonEventHandler(DeleteRow_MouseLeftButtonDown);
 
             returnVal.Children.Add(textbox);
             returnVal.Children.Add(deleteIcon);
@@ -328,6 +437,37 @@ namespace OsbleRubric
             return returnVal;
         }
 
+        //returns the row of a MouseEventArgs. If there is no row -1
+        private int getRow(MouseEventArgs e)
+        {
+            int returnVal = -1;
+            var eles = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(thisView), thisView.LayoutRoot);
+            foreach (FrameworkElement rd in eles)
+            {
+                if (rd is Border)
+                {
+                    returnVal = Grid.GetRow(rd);
+                }
+            }
+            return returnVal;
+        }
+
+        //returns the column of a MouseEventArgs. If there is no column -1
+        private int getColumn(MouseEventArgs e)
+        {
+            int returnVal = -1;
+            var eles = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(thisView), thisView.LayoutRoot);
+            foreach (FrameworkElement rd in eles)
+            {
+                if (rd is Border)
+                {
+                    returnVal = Grid.GetColumn(rd);
+                }
+            }
+            return returnVal;
+        }
+
+
         public Rubric GetView()
         {
             return thisView;
@@ -335,17 +475,33 @@ namespace OsbleRubric
 
         #region Events
 
-        private void Delete_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void DeleteRow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            removeRow();
+            customDataGrid.RemoveRow(getRow(e));
         }
 
         private void DeleteCol_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            removeCol();
+            customDataGrid.RemoveColumn(getColumn(e));
         }
-
-        //all the following are temp events
+        void addCol_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            customDataGrid.RemoveColumn(customDataGrid.BaseGrid.ColumnDefinitions.Count - 1);
+            customDataGrid.RemoveColumn(customDataGrid.BaseGrid.ColumnDefinitions.Count - 1);
+            createLevelColumn();
+            createAddButtonColumn();
+            createCommentColumn();
+            customDataGrid.updateBorders();
+        }
+        void addRow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //removes the last row (the add button), then adds the new criterion row, then adds the add button row back in
+            customDataGrid.RemoveRow(customDataGrid.BaseGrid.RowDefinitions.Count - 1);
+            createCriterionRow();
+            createAddButtonRow();
+            customDataGrid.updateBorders();
+        }
+        //all the following are temporary events
         private void TBlock1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             createCriterionRow();
@@ -358,25 +514,12 @@ namespace OsbleRubric
 
         private void Img2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            removeCol();
-            //e.GetPosition(thisView);
-            /*
-            var eles = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(thisView), thisView.LayoutRoot);
-            foreach (UIElement rd in eles)
-            {
-                if (rd is Grid)
-                {
-                    //(rd as Grid).RowDefinitions
-                }
-            }
-            */
-            //VisualTreeHelper.
+            MessageBox.Show("Columns: " + customDataGrid.BaseGrid.ColumnDefinitions.Count.ToString() + "Rows: " + customDataGrid.BaseGrid.RowDefinitions.Count.ToString());
         }
 
         private void TBlock2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            customDataGrid.BaseGrid.ColumnDefinitions.RemoveAt(2);
-            // myGrid.BaseGrid.ColumnDefinitions.RemoveAt(myGrid.BaseGrid.ColumnDefinitions.Count - 1);
+           
         }
 
         #endregion Events
