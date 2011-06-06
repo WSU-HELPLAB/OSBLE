5226b38f2ed7a2fb5242763503a8cf3577e443ab
jneufeld_cp <SND\jneufeld_cp>
2011-06-06 19:08:39 +0000

    Useless check-in.

--- trunk/web/OSBLE/Models/Assignments/Activities/SubmissionActivity.cs
+++ trunk/web/OSBLE/Models/Assignments/Activities/SubmissionActivity.cs
@@ -3,20 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace OSBLE.Models.Assignments.Activities
 {
-    public class SubmissionActivity : AssignmentActivity, IHasDeliverables
+    public class SubmissionActivity : StudioActivity
     {
         public SubmissionActivity()
             : base()
         {
-            Deliverables = new List<Deliverable>();
+
         }
-
-        [Display(Name = "Deliverables")]
-        public virtual ICollection<Deliverable> Deliverables { get; set; }
-
-        [Required]
-        [Display(Name = "Description")]
-        public string Description { get; set; }
 
         [Required]
         [Display(Name = "Is Team")]
