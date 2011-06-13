accb3bf59f4420b45c18be0b751d51d0408f33f6
Adam Carter <acarteas@users.noreply.github.com>
2011-06-13 18:58:21 +0000

    Created a uploader thread that handles sending files to the server.  It appears to be working quite well, but further testing is needed.  The current system also lacks sync functionality (only upload newer files).

--- Controls/UploadingModal.xaml.cs
+++ Controls/UploadingModal.xaml.cs
@@ -10,29 +10,116 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Data;
+using FileUploader.OsbleServices;
+using System.ComponentModel;
 
 namespace FileUploader.Controls
 {
     public partial class UploadingModal : ChildWindow
     {
+        UploaderThread uploader = new UploaderThread();
+
+        public DirectoryListing Listing
+        {
+            get
+            {
+                return uploader.Listing;
+            }
+            set
+            {
+                uploader.Listing = value;
+                UploadProgressBar.Maximum = uploader.NumberOfUploads;
+            }
+        }
+
+        public string AuthToken
+        {
+            get
+            {
+                return uploader.AuthToken;
+            }
+            set
+            {
+                uploader.AuthToken = value;
+            }
+        }
+
+        public int CourseId
+        {
+            get
+            {
+                return uploader.CourseId;
+            }
+            set
+            {
+                uploader.CourseId = value;
+            }
+        }
+
         public UploadingModal()
         {
             InitializeComponent();
+
+            //make sure that Listing is never a null reference
+            Listing = new DirectoryListing();
+
+            //event listeners
+            uploader.UploadComplete += new EventHandler(UploadComplete);
+            uploader.FileUploadBegin += new EventHandler<FileUploadBegineArgs>(FileUploadStart);
+            uploader.Cancelled += new EventHandler(uploader_Cancelled);
+            OkButton.Click += new RoutedEventHandler(OkButton_Click);
+            CancelButton.Click += new RoutedEventHandler(CancelButton_Click);
+
+            UploadProgressBar.Maximum = uploader.NumberOfUploads;
         }
 
-        private void OKButton_Click(object sender, RoutedEventArgs e)
+        void uploader_Cancelled(object sender, EventArgs e)
         {
-            this.DialogResult = true;
+            this.Dispatcher.BeginInvoke(delegate()
+            {
+                UploadingFile.Text = "Upload Canceled";
+                OkButton.IsEnabled = true;
+                CancelButton.IsEnabled = false;
+            }
+            );
+        }
+
+        public void BeginUpload()
+        {
+            OkButton.IsEnabled = false;
+            CancelButton.IsEnabled = true;
+            uploader.Start();
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            this.DialogResult = false;
+            uploader.RequestCancel();
         }
 
-        private void OKButton_Click_1(object sender, RoutedEventArgs e)
+        private void FileUploadStart(object sender, FileUploadBegineArgs e)
         {
+            this.Dispatcher.BeginInvoke(delegate()
+            {
+                UploadingFile.Text = e.FileToUpload;
+                UploadProgressBar.Value = (sender as UploaderThread).NumberOfUploadsCompleted;
+            }
+            );
+        }
 
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void UploadComplete(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(delegate()
+            {
+                UploadingFile.Text = "Upload Complete";
+                OkButton.IsEnabled = true;
+                CancelButton.IsEnabled = false;
+            }
+            );
         }
     }
 }
--- UploaderPage.xaml.cs
+++ UploaderPage.xaml.cs
@@ -25,8 +25,13 @@ namespace FileUploader
     public partial class UploaderPage : Page
     {
         private string authToken = "";
-        private bool FileRecieved;
         public string RootPath;
+
+        //reference to our web service
+        private UploaderWebServiceClient client = new UploaderWebServiceClient();
+
+        // stores 
+        private KeyValuePair<int, string> course;
 
         public string LocalPath
         {
@@ -47,17 +52,6 @@ namespace FileUploader
             }
         }
 
-        //reference to our web service
-        UploaderWebServiceClient syncedFiles = new UploaderWebServiceClient();
-
-        // Modal Syncing progress window
-        UploadingModal syncing = new UploadingModal();
-        // Modal Uploading progress window
-        UploadingModal uploading = new UploadingModal();
-
-        // Creating course key for upload location combo box
-        KeyValuePair<int, string> course;
-
         public UploaderPage(string authenticationToken)
         {
             InitializeComponent();
@@ -69,9 +63,8 @@ namespace FileUploader
             RootPath = LocalPath;
 
             //listeners for our web service
-            syncedFiles.GetFileListCompleted += new EventHandler<GetFileListCompletedEventArgs>(syncedFiles_GetFileListCompleted);
-            syncedFiles.SyncFileCompleted += new EventHandler<SyncFileCompletedEventArgs>(syncedFiles_SyncFileCompleted);
-            syncedFiles.GetValidUploadLocationsCompleted += new EventHandler<GetValidUploadLocationsCompletedEventArgs>(syncedFiles_GetValidUploadLocationsCompleted);
+            client.GetFileListCompleted += new EventHandler<GetFileListCompletedEventArgs>(GetFileListCompleted);
+            client.GetValidUploadLocationsCompleted += new EventHandler<GetValidUploadLocationsCompletedEventArgs>(GetValidUploadLocationsCompleted);
 
             //local event listeners
             SyncButton.Click += new RoutedEventHandler(SyncButton_Click);
@@ -84,37 +77,29 @@ namespace FileUploader
             DownButton.Click += new RoutedEventHandler(DownButton_Click);
 
             //get the remote server file list
-            syncedFiles.GetValidUploadLocationsAsync(authToken);
+            client.GetValidUploadLocationsAsync(authToken);
             
             //get the local files
-            LocalFileList.DataContext = BuildLocalDirectoryListing(LocalPath);
+            LocalFileList.DataContext = FileOperations.BuildLocalDirectoryListing(LocalPath);
         }
 
         void DownButton_Click(object sender, RoutedEventArgs e)
         {
             RemoteFileList.MoveSelectionDown();
-            syncedFiles.UpdateListingOrderAsync(RemoteFileList.DataContext, course.Key, authToken);
+            client.UpdateListingOrderAsync(RemoteFileList.DataContext, course.Key, authToken);
         }
 
-        void UpButton_Click(object sender, RoutedEventArgs e)
+        void GetFileListCompleted(object sender, GetFileListCompletedEventArgs e)
         {
-            RemoteFileList.MoveSelectionUp();
-            syncedFiles.UpdateListingOrderAsync(RemoteFileList.DataContext, course.Key, authToken);
+            RemoteFileList.DataContext = e.Result;
         }
 
-        void UploadLocation_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //very hack-ish at the moment, may need to revise
-            course = (KeyValuePair<int, string>)UploadLocation.SelectedValue;
-            syncedFiles.GetFileListAsync(course.Key, authToken);
-        }
-
-        void syncedFiles_GetValidUploadLocationsCompleted(object sender, GetValidUploadLocationsCompletedEventArgs e)
+        void GetValidUploadLocationsCompleted(object sender, GetValidUploadLocationsCompletedEventArgs e)
         {
             UploadLocation.Items.Clear();
             UploadLocation.ItemsSource = e.Result;
             UploadLocation.SelectedIndex = 0;
-        }
+        }        
 
         /// <summary>
         /// Returns the path of the last path on this machine that was synced with the server
@@ -161,8 +146,8 @@ namespace FileUploader
             FileList list = sender as FileList;
             list.ClearPreviousDirectories();
             DirectoryListing listing = list.DataContext;
-            LocalPath = listing.Name.Substring(0, listing.Name.LastIndexOf('\\'));
-            LocalFileList.DataContext = BuildLocalDirectoryListing(LocalPath);
+            LocalPath = LocalPath.Substring(0, LocalPath.LastIndexOf('\\'));
+            LocalFileList.DataContext = FileOperations.BuildLocalDirectoryListing(LocalPath);
         }
 
         /// <summary>
@@ -176,176 +161,11 @@ namespace FileUploader
             list.ClearPreviousDirectories();
             DirectoryListing listing = list.DataContext;
             LocalPath = Path.Combine(LocalPath, listing.Name);
-            LocalFileList.DataContext = BuildLocalDirectoryListing(LocalPath);
+            LocalFileList.DataContext = FileOperations.BuildLocalDirectoryListing(LocalPath);
         }
 
         void SendFileButton_Click(object sender, RoutedEventArgs e)
         {
-            uploading.LayoutUpdated += new EventHandler(uploading_LayoutUpdated);
-            uploading.Show();
-        }
-
-        void uploading_LayoutUpdated(object sender, EventArgs e)
-        {
-            uploading.LayoutUpdated -= new EventHandler(uploading_LayoutUpdated);
-            string relpath = "";
-            string selected = "";
-            DirectoryListing Listing = new DirectoryListing();
-            
-            /*****************************************************************
-            // needs to be set to the selected dir or file data context
-            Listing = this.LocalFileList.DataContext;
-
-            // need to assign selected file or dir to "selected"
-            selected = LocalPath;
-
-            //sets the amount of elements in the progress bar
-            uploading.UploadProgressBar.Maximum = 10000;
-            *****************************************************************/
-
-            traveseDirectories(selected, relpath, Listing);
-
-            // update the synced files Window
-            syncedFiles.GetFileListAsync(course.Key, authToken);
-        }
-
-        void SyncButton_Click(object sender, RoutedEventArgs e)
-        {
-            syncing.LayoutUpdated += new EventHandler(syncing_LayoutUpdated);
-            syncing.Show();
-            
-        }
-
-        void syncing_LayoutUpdated(object sender, EventArgs e)
-        {
-            syncing.LayoutUpdated -= new EventHandler(syncing_LayoutUpdated);
-            string relpath = "";
-            DirectoryListing Listing = new DirectoryListing();
-            Listing = this.LocalFileList.DataContext;
-
-            //sets the amount of elements in the progress bar
-            syncing.UploadProgressBar.Maximum = 10000;
-
-            traveseDirectories(LocalPath, relpath, Listing);
-
-            // update the synced files Window
-            syncedFiles.GetFileListAsync(course.Key, authToken);
-        }
-                
-        // solicites the server to create the directories and files in the tree
-        private void traveseDirectories(string path, string relative, DirectoryListing Listing)
-        {
-            SyncCurrentDirectories(Listing, relative);                          // creates the directories
-            int fileindex = 0;
-            foreach (string file in Directory.EnumerateFiles(path))
-            {
-                FileRecieved = false;
-                DateTime updated = Listing.Files[fileindex].LastModified;
-                SyncCurrentFile(file, updated);                                 // tells server to create file
-                fileindex++;
-            }
-            
-            foreach (string directory in Directory.EnumerateDirectories(path))
-            {
-                relative = directory.Substring(LocalPath.Length + 1);
-                Listing = BuildLocalDirectoryListing(directory);  // drops down a level
-                traveseDirectories(directory, relative, Listing);               // recursive call
-            } 
-        }
-
-        // prompts server to create all directories on the current level
-        void SyncCurrentDirectories(DirectoryListing level, string relative)
-        {
-            if (level.Directories.Count > 0)
-            {
-                syncedFiles.PrepCurrentPathAsync(level, relative, course.Key, authToken);
-            }
-        }
-
-        // Prompts the server to create the file
-        void SyncCurrentFile(string file, DateTime updated)
-        {
-            //Server Relative path
-            string relativepath = file.Substring(LocalPath.Length + 1);
-            
-            // updating the textblock with current file uploading Local relative path
-            syncing.UploadingFile.Text = file.Substring(RootPath.Length + 1);
-            syncing.UploadingFile.UpdateLayout();
-
-            //update progres bar (ie add a tick)
-            syncing.UploadProgressBar.Value++;
-            syncing.UploadProgressBar.UpdateLayout();
-            
-            
-            MemoryStream s = new MemoryStream();
-            StreamWriter writer = new StreamWriter(s);
-            writer.Write(file);
-            writer.Flush();
-
-            using (Stream stream = s)
-            {
-                byte[] data = new byte[stream.Length];
-                stream.Read(data, 0, (int)stream.Length);
-
-                syncedFiles.SyncFileAsync(relativepath, data, updated, course.Key, authToken);
-            }
-            // wait till the file is uploaded
-            //while (!FileRecieved)
-            //{
-            //    //sleep
-            //    TimeSpan waitTime = new TimeSpan(0, 0, 1);
-
-            //    Thread.Sleep(waitTime);
-            //}
-        }
-
-        // when one file is done being created it will set the bool to true
-        void syncedFiles_SyncFileCompleted(object sender, SyncFileCompletedEventArgs e)
-        {
-            //boolean file sync complete
-            FileRecieved = true;
-            
-        }
-        
-        public DirectoryListing BuildLocalDirectoryListing(string path)
-        {
-            DirectoryListing listing = new DirectoryListing();
-            listing.Directories = new ObservableCollection<DirectoryListing>();
-            listing.Files = new ObservableCollection<FileListing>();
-            listing.Name = path;
-
-            //The "..." always goes on top
-            listing.Directories.Add(new ParentDirectoryListing() { Name = "..." });
-
-            //apparently, some folders are restricted.  Use a try block to catch
-            //write exceptions
-            try
-            {
-                //add files first
-                foreach (string file in Directory.EnumerateFiles(path))
-                {
-                    FileListing fileListing = new FileListing();
-                    fileListing.LastModified = File.GetLastWriteTime(file);
-                    fileListing.Name = Path.GetFileName(file);
-                    listing.Files.Add(fileListing);
-                }
-
-                //add other directories
-                foreach (string folder in Directory.EnumerateDirectories(path))
-                {
-                    DirectoryListing dList = new DirectoryListing();
-                    dList.Files = new ObservableCollection<FileListing>();
-                    dList.Directories = new ObservableCollection<DirectoryListing>();
-                    dList.LastModified = Directory.GetLastWriteTime(folder);
-                    dList.Name = folder.Substring(folder.LastIndexOf('\\') + 1);
-                    listing.Directories.Add(dList);
-                }
-            }
-            catch (Exception ex)
-            {
-                //something went wrong, oh well (for now)
-            }
-            return listing;
         }
 
         /// <summary>
@@ -365,15 +185,28 @@ namespace FileUploader
             }
         }
 
-        // Executes when the user navigates to this page.
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        void SyncButton_Click(object sender, RoutedEventArgs e)
         {
+            DirectoryListing listing = FileOperations.BuildLocalDirectoryListing(LocalPath, false, true);
+            UploadingModal uploader = new UploadingModal();
+            uploader.Listing = listing;
+            uploader.CourseId = course.Key;
+            uploader.AuthToken = authToken;
+            uploader.BeginUpload();
+            uploader.Show();
         }
 
-        void syncedFiles_GetFileListCompleted(object sender, GetFileListCompletedEventArgs e)
+        void UpButton_Click(object sender, RoutedEventArgs e)
         {
-            RemoteFileList.DataContext = e.Result;
-            syncing.Close();
+            RemoteFileList.MoveSelectionUp();
+            client.UpdateListingOrderAsync(RemoteFileList.DataContext, course.Key, authToken);
+        }
+
+        void UploadLocation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //very hack-ish at the moment, may need to revise
+            course = (KeyValuePair<int, string>)UploadLocation.SelectedValue;
+            client.GetFileListAsync(course.Key, authToken);
         }
     }
 }
