44a1f00c74276c7b1001a517eaa4fa06e5f1e577
jneufeld_cp <SND\jneufeld_cp>
2011-06-14 18:56:31 +0000

    Removed IsDraft flag (for now)

--- trunk/web/OSBLE/Models/Assignments/StudioAssignment.cs
+++ trunk/web/OSBLE/Models/Assignments/StudioAssignment.cs
@@ -15,10 +15,6 @@ namespace OSBLE.Models.Assignments
         [Display(Name = "Description")]
         public string Description { get; set; }
 
-        [Required]
-        [Display(Name = "Draft Assignment")]
-        public bool IsDraft { get; set; }
-
         [NotMapped]
         public override bool AppearsInAssignmentList
         {
