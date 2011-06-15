dce4fc77108c916f88678c45974291749dd7b229
adamcarter_cp <SND\adamcarter_cp>
2011-06-15 18:34:35 +0000

    As requested by Chris, removed most of the buttons and replaced them with right-click menu items.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileUploader")]
+[assembly: AssemblyTitle("OSBLE File Uploader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FileUploader")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Washington State University")]
+[assembly: AssemblyProduct("OSBLE File Uploader")]
+[assembly: AssemblyCopyright("Copyright © Washington State University 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
