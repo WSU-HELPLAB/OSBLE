220eeb1df239f5adb7c733846a6583b2248dec31
jtgrimes_cp <SND\jtgrimes_cp>
2011-06-22 23:15:44 +0000

    Added tooltips to more locations (80% complete). Also tweaks to jquery-tooltip.css.

--- trunk/web/OSBLE/Models/ToolTips.cs
+++ trunk/web/OSBLE/Models/ToolTips.cs
@@ -143,6 +143,104 @@ namespace OSBLE.Models
 
         #endregion
 
+        
+        #region RosterToolTips
+
+            public static string RosterAddUpdate = "Click this link to view the interface for adding users to the database either using a file or manually.";
+            public static string RosterSingleUser = "This section allows you to add an individual user to the class, either by their school identification number or their email.";
+            public static string RosterImport = "This section allows you to import a list of users from a comma-separated (<em>.csv</em>) file to the class.";
+            public static string RosterQuickFilter = "Type in all or part of a user's name to filter this view to show only users who match that criteria.";
+            public static string RosterCommunityUser = "";
+
+            // because in /Views/Roster/Index.cshtml there is a dynamically generated section, this data structure must be used
+            public static IDictionary<string, string> RosterRolesDictionary = new Dictionary<string, string> { 
+                {"Observer","Observers are allowed to view the course information without being allowed to participate as either instructors or students are."},
+                {"Instructor","Instructors are the users who have control over the grades, assignments, and other activities in a course."},
+                {"Student","Students are the individuals who participate in the course for turning in assignments, receiving grades, and other activities."},
+                {"TA","Teaching assistant who helps with grading assignments."},
+                {"Moderator","Moderators are...?"},
+                {"Leader","Community leader...?"},
+                {"Participant","Community participant...?"} };
+
+            public static string RosterUserName = "This is the name this user will be publicly identified by in mail, posts, assignments, and other content.";
+            public static string RosterEmail = "This must be the email address this user will use with OSBLE.";
+            public static string RosterSchoolID = "Input the identification number for this user to indicate who is being added to this course.";
+            public static string RosterRole = "Select what role this person has for this course. ";
+            public static string RosterSection = "This is the section (<em>Enter 0 to add this user to all sections</em>).";
+            public static string RosterSave = "To save the changes made on this page, you must click this submit button.";
+            public static string RosterBack = "To discard changes made here and go back to the main roster page, click this link.";
+
+        #endregion
+        
+
+        #region CommunityToolTips
+        
+            public static string CommunityName = "This is the full name of this community.";
+            public static string CommunityDescription = "Describe what this community is and why you made it here. ";
+            public static string CommunityNickname = "This is a 3-4 letter nickname (identifier) for thisw community to be shown where there is not enough space to display the whole name of the community. Ideally, this nickname will be connected with the name somehow, such as an acronym or abbreviation.";
+            public static string CommunityAllowPosting = "If you want participants and not just leaders to be able to post events to this community, you must check the box.";
+            public static string CommunityCalendarEvents = "For every user that is a part of this group, events will be displayed on their calendar this many weeks in advance.";
+            public static string CommunitySave = "You must click the button to save any changes you made on this page.";
+
+        #endregion
+
+        
+        #region MailToolTips
+
+            public static string MailNewMessage = "Use the search box below to create a new message to a user of OSBLE.";
+            public static string MailRecipient = "Type in some letters in the user's name and a list of users that match that search term will pop up. Select the desired user from that list to continue and compose your message.";
+            public static string MailInbox = "Your mail inbox shows all messages that have been sent to you. Messages that have yet to be read are bold. There is an (X) on the right of each message to allow you to delete messages.";
+            public static string MailOutbox = "Your mail outbox shows all messages that you have sent to other users. Messages that have yet to be read are bold. There is an (X) on the right of each message to allow you to delete messages.";
+
+            public static string MailCreateTo = "This is the person this message will be sent to. If you want to change the recipient, you must create a new message. This message will not be saved if you clikc the back button.";
+            public static string MailCreateSubject = "This is the title of the message you are sending.";
+            public static string MailCreateMessage = "This is the body of the message you are sending.";
+            public static string MailSave = "In order to send this message, you must click here. If you click any other link, this message will not be saved.";
+            public static string MailBack = "If you don't want to send this message, click here.";
+
+            public static string MailDelete = "Click here to permanently delete this message. <em>Note: This action cannot be undone.</em>";
+            public static string MailReply = "Click here to reply to this message. The previous message will be quoted in the message body. If you do not want to quote the original message, you can delete it while editing the message.";
+
+        #endregion
+
+        
+        #region EventToolTips
+
+            public static string EventCreate = "Click here to create a new event.";
+            public static string EventPastEvents = "These are events that have already occurred that you were invited to or part of. (Past events are based on the date and time specified in creation).";
+            public static string EventCurrentEvents = "These are events that have yet to occur that you were invited to or part of.";
+
+            public static string EventTitle = "This is the title this event will have. This is what will show up on the event information box as bold";
+            public static string EventDescription = "Use this section to completely describe this event, if you so desire. If there is not a section for a specific piece of information that you feel is important to include about this event, such as the length of time it will be or if there is a specific meeting place, mention it in this section.";
+            public static string EventDate = "This is the day of the year that the event will occur.";
+            public static string EventTime = "This is the time of day that the event will occur.";
+            public static string EventLinkTitle = "If you want to have a link to another web location, this is the title that the hyperlink will have, rather than the exact web address. If you want the hyperlink to be shown as its web address, place the address in this section and in the link section below.";
+            public static string EventLink = "Place the web location you want to link to in this section.";
+            public static string EventSave = "You must click here to save any modifications made on this page.";
+            public static string EventBack = "If you don't want to save any of the modifications you made on this page, or if you accidentally managed to come to this page, click here.";
+
+        #endregion
+
+        
+        #region AdminToolTips
+
+            public static string AdminTitle = "In this section, you can edit, delete, view the details of, or impersonate a user using the links next to their name. When you impersonate a user, you see what they would when they log in to OSBLE, such as messages, notifications, and assignment feedback, among other things. This feature is useful for checking how OSBLE looks and feels for users to ensure that they can utilize this system to its fullest.";
+
+            public static string AdminUserName = "This is the name that these individuals use to log in to the system. It must be their email address.";
+            public static string AdminUserSchool = "This is the school that this user is associated with.";
+            public static string AdminUserID = "This is the identification number of the users. These are typically used by the system itself rather than users. ";
+            public static string AdminUserFirstName = "This name and the last name will be displayed when users interact.";
+            public static string AdminUserLastName = "This name and the first name will be displayed when users interact.";
+            public static string AdminUserIsAdmin = "This checkbox indicates whether or not this user has administrator permissions on OSBLE.";
+            public static string AdminUserIsCourseCreator = "This checkbox indicates whether or not the user can create courses.";
+
+            public static string AdminSave = "You must click this button in order to save any changes you made above.";
+            public static string AdminBack = "If you don't want to save any changes made on this page or you just want to navigate back to the list of users, click here.";
+            public static string AdminEdit = "You can edit features of this user by following this link.";
+            public static string AdminDelete = "Click here to delete this user. <em>Note: This action can NOT be undone and this button offers no additional confirmation window.</em>";
+
+        #endregion
+
 
         /*
          * TEMPLATE FOR ADDING TOOLTIPS (FOR ORGANIZATIONAL PURPOSES)
