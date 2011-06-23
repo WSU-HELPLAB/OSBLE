fe6946061b9a92ce51d0dd2e546f2184690d6b37
adamcarter_cp <SND\adamcarter_cp>
2011-06-23 18:14:49 +0000

    -Course links with a ".link" extension will be treated as web URLs.

--- trunk/web/OSBLE/Controllers/FileHandlerController.cs
+++ trunk/web/OSBLE/Controllers/FileHandlerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace OSBLE.Controllers
 {
@@ -20,9 +21,32 @@ namespace OSBLE.Controllers
         {
             string rootPath = FileSystem.GetCourseDocumentsPath(courseId);
 
+            //AC: At some point, it might be a good idea to document these hacks
+
             //assume that commas are used to denote directory hierarchy
             rootPath += "\\" + filePath.Replace(',', '\\');
-            return new FileStreamResult(FileSystem.GetDocumentForRead(rootPath), "application/octet-stream");
+
+            //if the file ends in a ".link", then we need to treat it as a web link
+            if (rootPath.Substring(rootPath.LastIndexOf('.') + 1).ToLower().CompareTo("link") == 0)
+            {
+                string url = "";
+
+                //open the file to get at the link stored inside
+                using (TextReader tr = new StreamReader(rootPath))
+                {
+                    url = tr.ReadLine();
+                }
+                Response.Redirect(url);
+
+                //this will never be reached, but the function requires an actionresult to be returned
+                return Json("");
+            }
+            else
+            {
+                //else just return the file
+                return new FileStreamResult(FileSystem.GetDocumentForRead(rootPath), "application/octet-stream");
+            }
+
         }
 
     }
