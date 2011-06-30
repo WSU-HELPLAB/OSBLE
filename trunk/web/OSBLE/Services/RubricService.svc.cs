4e0b6bd8cadd1a0ad301c21d722a663a18ba9fbb
adamcarter_cp <SND\adamcarter_cp>
2011-06-30 17:13:59 +0000

    -Added a fake rubric service to facilitate model sharing between OSBLE and the Rubric project -Replies to community threads will now show the author's title.

---
+++ trunk/web/OSBLE/Services/RubricService.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using OSBLE.Models.Courses.Rubrics;
+namespace OSBLE.Services
+{
+    [ServiceContract(Namespace = "")]
+    [SilverlightFaultBehavior]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class RubricService
+    {
+        [OperationContract]
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        [OperationContract]
+        public CellDescription GetFakeCellDescription()
+        {
+            return new CellDescription();
+        }
+
+        [OperationContract]
+        public Criterion GetFakeCriterion()
+        {
+            return new Criterion();
+        }
+
+        [OperationContract]
+        public Level GetFakeLevel()
+        {
+            return new Level();
+        }
+
+        [OperationContract]
+        public Rubric GetFakeRubric()
+        {
+            return new Rubric();
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
