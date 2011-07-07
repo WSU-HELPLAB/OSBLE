d83d3d7e785e8ca998b67a23de7d9ae44d5d2d20
adamcarter_cp <SND\adamcarter_cp>
2011-07-07 22:48:21 +0000

    -More work on the rubric evaluation tool.  Almost done!

--- trunk/web/OSBLE/Controllers/RubricController.cs
+++ trunk/web/OSBLE/Controllers/RubricController.cs
@@ -8,16 +8,48 @@ using System.Web.Mvc;
 using OSBLE.Models.Courses.Rubrics;
 using OSBLE.Models;
 using OSBLE.Models.ViewModels;
+using OSBLE.Attributes;
+using OSBLE.Models.Courses;
+using OSBLE.Models.Assignments;
+using OSBLE.Models.Users;
 
 namespace OSBLE.Controllers
 { 
     public class RubricController : OSBLEController
     {
+        [CanGradeCourse]
         public ActionResult Index()
         {
-            Rubric rubric = db.Rubrics.Find(1);
+            Rubric rubric = db.Rubrics.Find(2);
             RubricViewModel viewModel = new RubricViewModel();
             viewModel.Rubric = rubric;
+
+            //assignments are storied within categories, which are found within
+            //the active course.  
+            List<AbstractAssignment> assignments = new List<AbstractAssignment>();
+            foreach (Category cat in (activeCourse.Course as Course).Categories)
+            {
+                foreach (AbstractAssignment assignment in cat.Assignments)
+                {
+                    assignments.Add(assignment);
+                }
+            }
+            viewModel.Assignments = new SelectList(assignments, "ID", "Name");
+
+            List<int> sections = (from cu in db.CoursesUsers
+                                  where cu.CourseID == activeCourse.CourseID
+                                  select cu.Section).Distinct().ToList();
+            viewModel.Sections = new SelectList(sections);
+
+            var users = (from cu in db.CoursesUsers
+                                      where cu.CourseID == activeCourse.CourseID
+                                      &&
+                                      cu.Section == viewModel.SelectedSection
+                                      &&
+                                      cu.CourseRole.CanSubmit
+                                      select new {ID = cu.UserProfile.ID, Name = cu.UserProfile.LastName + ", " + cu.UserProfile.FirstName}).ToList();
+            viewModel.Students = new SelectList(users, "ID", "Name");
+
             return View(viewModel);
         }
     }
--- trunk/web/OSBLE/Models/ViewModels/RubricViewModel.cs
+++ trunk/web/OSBLE/Models/ViewModels/RubricViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using OSBLE.Models.Courses.Rubrics;
+using OSBLE.Models.Assignments;
+using OSBLE.Models.Users;
+using System.Web.Mvc;
 
 namespace OSBLE.Models.ViewModels
 {
@@ -12,9 +15,16 @@ namespace OSBLE.Models.ViewModels
         {
             Rubric = new Rubric();
             Evaluation = new RubricEvaluation();
+            SelectedSection = 0;
         }
 
         public Rubric Rubric { get; set; }
         public RubricEvaluation Evaluation { get; set; }
+        public AbstractAssignment SelectedAssignment { get; set; }
+        public SelectList Assignments { get; set; }
+        public SelectList Students { get; set; }
+        public UserProfile SelectedStudent { get; set; }
+        public SelectList Sections { get; set; }
+        public int SelectedSection { get; set; }
     }
 }
