0ce35f34584ee05170cd2fe2e55b856a2699957a
adamcarter_cp <SND\adamcarter_cp>
2011-07-15 17:38:58 +0000

    -Removed the ability to download files from the file browser.  A recent change in OSBLE requires that users be authenticated in OSBLE prior to downloading a file.  Since OOB apps run out of the browser, we can't authenticate with OSBLE.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Washington State University")]
 [assembly: AssemblyProduct("OSBLE File Uploader")]
-[assembly: AssemblyCopyright("Copyright © Washington State University 2011")]
+[assembly: AssemblyCopyright("Copyright © 2011 Washington State University")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.11.7.0")]
-[assembly: AssemblyFileVersion("0.11.7.0")]
+[assembly: AssemblyVersion("0.11.7.1")]
+[assembly: AssemblyFileVersion("0.11.7.1")]
