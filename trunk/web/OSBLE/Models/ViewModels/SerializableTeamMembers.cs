b4f86e914be0ae4880debe238afb56a46cda210c
adamcarter_cp <SND\adamcarter_cp>
2011-08-08 17:42:48 +0000

    -Added a compareTo function to the SerializableTeamMembers class.

--- trunk/web/OSBLE/Models/ViewModels/SerializableTeamMembers.cs
+++ trunk/web/OSBLE/Models/ViewModels/SerializableTeamMembers.cs
@@ -1,9 +1,10 @@
-﻿namespace OSBLE.Models.ViewModels
+﻿using System;
+namespace OSBLE.Models.ViewModels
 {
     /// <summary>
     /// This must match the SerializableTeamMembers class that the TeamCreation (silverlight tool) has exactly
     /// </summary>
-    public class SerializableTeamMember
+    public class SerializableTeamMember : IComparable
     {
         public bool isUser { get; set; }
 
@@ -22,5 +23,56 @@
         public int InTeamID { get; set; }
 
         public string InTeamName { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            SerializableTeamMember other = obj as SerializableTeamMember;
+            if (other == null)
+            {
+                return -1;
+            }
+
+            if (isUser != other.isUser)
+            {
+                return -1;
+            }
+
+            if (UserID != other.UserID)
+            {
+                return -1;
+            }
+
+            if (TeamID != other.TeamID)
+            {
+                return -1;
+            }
+
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                Name.CompareTo(other.Name);
+            }
+
+            if (Section != other.Section)
+            {
+                return -1;
+            }
+
+            if (IsModerator != other.IsModerator)
+            {
+                return -1;
+            }
+
+            if (Subbmitted != other.Subbmitted)
+            {
+                return -1;
+            }
+
+            if (InTeamID != other.InTeamID)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
     }
 }
