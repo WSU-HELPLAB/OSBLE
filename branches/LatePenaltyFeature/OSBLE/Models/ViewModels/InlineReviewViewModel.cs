dd0efc7f669c0b0e621c0da7d999ec28fff4646c
adamcarter_cp <SND\adamcarter_cp>
2012-01-23 23:32:24 +0000

    -deleted old branch.

--- branches/LatePenaltyFeature/OSBLE/Models/ViewModels/InlineReviewViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OSBLE.Models.ViewModels
-{
-    public class InlineReviewViewModel
-    {
-        public SilverlightObject ReviewInterface { get; set; }
-    }
-}
--- branches/LatePenaltyFeature/OSBLE/Scripts/osble.assignment_list.js
+++
@@ -1,5 +0,0 @@
-﻿$(function () {
-    $('.assignment_accordion').accordion({ collapsible: true, header: 'div.assignment_header', autoHeight: false, active: false });
-
-    $('.assignment_header').unbind('focus');
-});
