19452c296fa8b7169403f1de07a054483db1557b
mario87_cp <SND\mario87_cp>
2012-03-28 01:03:42 +0000

    Added regex to parse assignment description and make it a link. Note: currently only works with  1 link, needs work.

--- trunk/web/OSBLE/Controllers/AssignmentController.cs
+++ trunk/web/OSBLE/Controllers/AssignmentController.cs
@@ -15,6 +15,7 @@ using System.Data.Entity.Validation;
 using System.Diagnostics;
 using OSBLE.Models.Courses.Rubrics;
 using OSBLE.Models.DiscussionAssignment;
+using System.Text.RegularExpressions;
 
 namespace OSBLE.Controllers
 {
@@ -348,7 +349,7 @@ namespace OSBLE.Controllers
                         AssignmentDetailsList.Add(new AssignmentDetailsViewModel(null, subTime, team.Team, postCount, replyCount));
                     }
                 }
-                
+
                 //Just giving the first or defaults teams ID, the instructor link to the rubric won't be specific.
                 ViewBag.TeamID = 0;
                 if(assignment.AssignmentTeams.Count > 0)
@@ -413,7 +414,28 @@ namespace OSBLE.Controllers
                 }
             }
             ViewBag.TeamMembers = cuList; //null if no team members
-            
+
+
+            //Parsing description looking for a link in thr format [url:WordToLink|http://www.placetolink.com]
+            //Then replacing it with html to make it a link.
+            Match match = Regex.Match(assignment.AssignmentDescription, @"\[url:[\w\W]*\|[\w\W]*]");
+            if (match.Length > 0)
+            {
+                string parsedDescription = assignment.AssignmentDescription.Remove(match.Index, match.Length);
+                Match match2 = Regex.Match(match.Value, @"\|[\W\w]*]");
+                string url = match2.Value;
+                url = url.Substring(1, match2.Length - 2);
+                match2 = Regex.Match(match.Value, @"\:[\W\w]*\|");
+                string label = match2.Value;
+                label = label.Substring(1, match2.Length - 2);
+                string rawHtml = "<a href=\"" + url + "\">" + label + "</a>";
+                parsedDescription = parsedDescription.Insert(match.Index, rawHtml);
+                ViewBag.RawHtmlDescription = parsedDescription;
+            }
+            else
+            {
+                ViewBag.RawHtmlDescription = assignment.AssignmentDescription;
+            }
 
             //MG: getting a list of the deliverables to list for assignment details. 
             List<string[]> fileTypes = new List<string[]>();
