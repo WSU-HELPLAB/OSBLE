5047e53dc3381a973fa57f9eff53b0d648460ed5
mattrfk_cp <SND\mattrfk_cp>
2012-06-04 18:45:54 +0000

    -Added CriticalReviewDiscussionTeams.cs to osble->areas->controllers

---
+++ trunk/web/OSBLE/Areas/AssignmentWizard/Controllers/CriticalReviewDiscussionTeams.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OSBLE.Models.Assignments;
+
+namespace OSBLE.Areas.AssignmentWizard.Controllers
+{
+    public class CriticalReviewDiscussionTeams : WizardBaseController
+    {
+
+        public override string ControllerName
+        {
+            get { return "CriticalReviewDiscussionTeams"; }
+        }
+
+        public override string PrettyName
+        {
+            get
+            {
+                return "CR Discussion Teams";
+            }
+        }
+
+        public override string ControllerDescription
+        {
+            get { return "CriticalReviewDiscussionTeams"; }
+        }
+
+        public override bool IsRequired
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override WizardBaseController Prerequisite
+        {
+            get { return new PreviousAssignmentController(); }
+        }
+
+        public override ICollection<AssignmentTypes> ValidAssignmentTypes
+        {
+            get
+            {
+                List<AssignmentTypes> prereqs = new List<AssignmentTypes>();
+
+                prereqs.Add(AssignmentTypes.CriticalReviewDiscussion);
+
+                return prereqs;
+            }
+        }
+    }
+}
