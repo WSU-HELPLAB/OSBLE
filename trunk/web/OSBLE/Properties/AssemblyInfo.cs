941957dc6a182b67d2bbf57a08eb9d0a45b63911
adamcarter_cp <SND\adamcarter_cp>
2012-09-26 21:38:32 +0000

    -Reworked the account activation process to hopefully be more user friendly and less error prone.

--- trunk/web/OSBLE/Properties/AssemblyInfo.cs
+++ trunk/web/OSBLE/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor
 //      Patch
 //      Codeplex Check-in Number
-[assembly: AssemblyVersion("1.2.7.18754")]
-[assembly: AssemblyFileVersion("1.2.7.18754")]
+[assembly: AssemblyVersion("1.2.8.18831")]
+[assembly: AssemblyFileVersion("1.2.8.18831")]
