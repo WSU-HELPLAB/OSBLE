ef3ef435b3fc2f02edea3ea1acdcc437bfe440ff
adamcarter_cp <SND\adamcarter_cp>
2013-04-15 17:06:45 +0000

    -fixed a null reference exception that occurs when administrators attempt to impersonate a user that has not yet activated his or her account.

--- trunk/web/OSBLE/Properties/AssemblyInfo.cs
+++ trunk/web/OSBLE/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor
 //      Patch
 //      Codeplex Check-in Number
-[assembly: AssemblyVersion("13.3.25.24216")]
-[assembly: AssemblyFileVersion("13.3.25.24216")]
+[assembly: AssemblyVersion("13.4.8.24493")]
+[assembly: AssemblyFileVersion("13.4.8.24493")]
