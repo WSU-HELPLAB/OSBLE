faf96f99aa915c40d94d9a4281152e41c5b384dd
adamcarter_cp <SND\adamcarter_cp>
2013-05-22 17:04:17 +0000

    -Added help / tutorial section.

---
+++ trunk/web/OSBLE/Controllers/HelpController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSBLE.Controllers
+{
+    public class HelpController : OSBLEController
+    {
+        //
+        // GET: /Help/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCourse()
+        {
+            return View();
+        }
+
+        public ActionResult AddingStudents()
+        {
+            return View();
+        }
+
+        public ActionResult AddingOthers()
+        {
+            return View();
+        }
+
+        public ActionResult CreateBasicAssignment()
+        {
+            return View();
+        }
+
+        public ActionResult CreateReviewAssignment()
+        {
+            return View();
+        }
+
+        public ActionResult CreateReviewDiscussion()
+        {
+            return View();
+        }
+
+        public ActionResult AddWebLinks()
+        {
+            return View();
+        }
+
+        public ActionResult Gradebook()
+        {
+            return View();
+        }
+
+        public ActionResult FileUploader()
+        {
+            return View();
+        }
+    }
+}
