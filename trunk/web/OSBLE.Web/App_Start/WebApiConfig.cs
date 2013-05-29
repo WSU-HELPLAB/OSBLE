bb7fc74ff971547bab41c82a7d754d3b1b656db6
adamcarter_cp <SND\adamcarter_cp>
2013-05-29 18:41:42 +0000

    -added odata libraries to project

--- trunk/web/OSBLE.Web/App_Start/WebApiConfig.cs
+++ trunk/web/OSBLE.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using OSBLE.Models.Assignments;
+using OSBLE.Models.Courses;
+using OSBLE.Models.Courses.Rubrics;
+using OSBLE.Models.HomePage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
 
 namespace OSBLE.Web
 {
@@ -14,6 +19,27 @@ namespace OSBLE.Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            ODataModelBuilder modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<Course>("Courses");
+            modelBuilder.EntitySet<CourseMeeting>("CourseMeetings");
+            modelBuilder.EntitySet<CourseBreak>("CourseBreaks");
+            modelBuilder.EntitySet<Assignment>("Assignments");
+            modelBuilder.EntitySet<Rubric>("Rubric");
+            modelBuilder.EntitySet<CommentCategoryConfiguration>("CommentCategory");
+            modelBuilder.EntitySet<Deliverable>("Deliverables");
+            modelBuilder.EntitySet<AssignmentTeam>("AssignmentTeams");
+            modelBuilder.EntitySet<DiscussionTeam>("DiscussionTeams");
+            modelBuilder.EntitySet<DiscussionSetting>("DiscussionSettings");
+            modelBuilder.EntitySet<ReviewTeam>("ReviewTeams");
+            modelBuilder.EntitySet<CriticalReviewSettings>("CriticalReviewSettings");
+            modelBuilder.EntitySet<TeamEvaluationSettings>("TeamEvaluationSettings");
+            modelBuilder.EntitySet<AbetAssignmentOutcome>("AbetOutcomes");
+            modelBuilder.EntitySet<Event>("AssociatedEvent");
+
+
+            Microsoft.Data.Edm.IEdmModel model = modelBuilder.GetEdmModel();
+            config.Routes.MapODataRoute("ODataRoute", "odata", model);
         }
     }
 }
