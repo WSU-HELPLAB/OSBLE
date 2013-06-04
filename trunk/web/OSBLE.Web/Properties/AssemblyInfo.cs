56567f2bf635d8c58f9c08ea5ddb9d37201980d0
adamcarter_cp <SND\adamcarter_cp>
2013-06-04 17:24:54 +0000

    -fixed assignment wizard area css links (didn't I do this already?)

--- trunk/web/OSBLE.Web/Properties/AssemblyInfo.cs
+++ trunk/web/OSBLE.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.6.4.26538")]
-[assembly: AssemblyFileVersion("13.6.4.26538")]
+[assembly: AssemblyVersion("13.6.4.26539")]
+[assembly: AssemblyFileVersion("13.6.4.26539")]
