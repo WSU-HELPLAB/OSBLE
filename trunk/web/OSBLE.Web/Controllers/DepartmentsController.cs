8f8fb863f391123937c7a101b53edf3729be0301
evanolds_cp <SND\evanolds_cp>
2013-06-05 21:46:12 +0000

    Added "departments administration" link to the admin page and an accompanying department list editor (just a text area). The department list that's created from this interface is required for ABET tagging.

---
+++ trunk/web/OSBLE.Web/Controllers/DepartmentsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using OSBLE.Models.HomePage;
+using OSBLE.Attributes;
+using OSBLE.Models;
+
+namespace OSBLE.Controllers
+{
+    public class DepartmentsController : OSBLEController
+    {
+        //
+        // GET: /Departments/
+        [OsbleAuthorize]
+        [IsAdmin]
+        public ActionResult Index()
+        {
+            // The list of departments is stored in a plain text file in the 
+            // root directory of the file system.
+            string[] depts = null;
+            OSBLE.Models.FileSystem.FileSystem fs = 
+                new Models.FileSystem.FileSystem();
+            string path = fs.GetPath();
+            if (System.IO.Directory.Exists(path))
+            {
+                path = System.IO.Path.Combine(path, "departments.txt");
+                if (System.IO.File.Exists(path))
+                {
+                    depts = System.IO.File.ReadAllLines(path);
+                }
+            }
+            ViewBag.DepartmentList = depts;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save()
+        {
+            // Get the list of departments from the form data
+            string allDepts = Request.Form["taDepts"];
+
+            if (null != allDepts)
+            {
+                OSBLE.Models.FileSystem.FileSystem fs = 
+                new Models.FileSystem.FileSystem();
+                string path = fs.GetPath();
+                if (System.IO.Directory.Exists(path))
+                {
+                    path = System.IO.Path.Combine(path, "departments.txt");
+                    System.IO.File.WriteAllText(path, allDepts);
+                }
+            }
+
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
