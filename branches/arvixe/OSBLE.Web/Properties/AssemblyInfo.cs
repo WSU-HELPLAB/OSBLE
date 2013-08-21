a7e33112d54a8f5fc8353c7fc83c60bb9e485b3a
adamcarter_cp <SND\adamcarter_cp>
2013-08-21 15:32:52 +0000

    -reverted to the old, non timezone-based time output for events and deadlines (quick fix, will address for real later)

--- branches/arvixe/OSBLE.Web/Properties/AssemblyInfo.cs
+++ branches/arvixe/OSBLE.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.7.2.27439")]
-[assembly: AssemblyFileVersion("13.7.2.27439")]
+[assembly: AssemblyVersion("13.8.20.29189")]
+[assembly: AssemblyFileVersion("13.8.20.29189")]
