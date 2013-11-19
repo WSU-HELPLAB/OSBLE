6b487a6aca5084bb35bf74a6625729c182aa6d0d
Young Chon <djenjin@gmail.com>
2013-11-19 20:53:43 +0000

    YC: created classes for whitetables and whitetableusers, this is before anyediting of cshtml files for new white table usage.

---
+++ OSBLE.Library/Models/AbstractCourses/Course/CourseWhiteTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OSBLE.Models.AbstractCourses.Course
+{
+    public class CourseWhiteTable
+    {
+        [Required]
+        [Key]
+        public int ID
+        {
+            set;
+            get;
+        }
+        [Required]
+        public int Section
+        {
+            get;
+            set;
+
+        }
+        [Required]
+        public virtual IList<WhiteTableUser> entries { set; get; }
+
+
+        public CourseWhiteTable ()
+            : base()
+        {
+            
+        }
+
+        
+    }
+}
