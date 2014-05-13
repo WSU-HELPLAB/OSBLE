8840f3525c054d9e555bd6b04ea78a538440ceef
Young Chon <djenjin@gmail.com>
2014-05-13 21:51:48 +0000

    yc: updated course creation, now includes timezone updates.

--- OSBLE.Web/Controllers/CourseController.cs
+++ OSBLE.Web/Controllers/CourseController.cs
@@ -72,7 +72,7 @@ namespace OSBLE.Controllers
 
                     cm.StartTime = cm.StartTime.AddMinutes(utcOffset);
                     cm.EndTime = cm.EndTime.AddMinutes(utcOffset);
-                    course.TimeZoneOffset = cm.TimeZoneOffset = Convert.ToInt32(Request.Params["meeting_timezone"]);
+
                     //Check to see if the utc offset will change the day if so adjust the Meeting's date
                     if (beforeUtcStartTime.DayOfYear != cm.StartTime.DayOfYear)
                     {
@@ -253,6 +253,7 @@ namespace OSBLE.Controllers
                 {
                 }
 
+                course.TimeZoneOffset = Convert.ToInt32(Request.Params["course_timezone"]);
                 createMeetingTimes(course, utcOffset);
                 createBreaks(course);
 
