6573c68b6123feb466a5cf8eb9a0625c29eecd38
ychon_cp <SND\ychon_cp>
2014-07-01 02:18:31 +0000

    yc: timetesting controller created for osble live server testing purposes

---
+++ branches/arvixe/OSBLE.Web/Controllers/TestController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Configuration;
+using OSBLE.Attributes;
+using OSBLE.Models;
+using OSBLE.Models.Assignments;
+using OSBLE.Models.Courses;
+using OSBLE.Models.Users;
+using OSBLE.Models.ViewModels;
+using OSBLE.Models.HomePage;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Data;
+using OSBLE.Utility;
+
+
+namespace OSBLE.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+
+            ViewBag.datetime = DateTime.Now.ToString();
+            ViewBag.utcdateTime = DateTime.UtcNow.ToString();
+            DateTime d = DateTime.Now;
+            ViewBag.serverTimezone = TimeZone.CurrentTimeZone.StandardName.ToString();
+            ViewBag.IsDaylightSavings = TimeZone.CurrentTimeZone.IsDaylightSavingTime(d).ToString();
+
+            TimeZone curTimezone = TimeZone.CurrentTimeZone;
+            TimeSpan currentOffset = curTimezone.GetUtcOffset(DateTime.Now);
+            TimeSpan classTimeZone;
+
+            ViewBag.CalculatedCurrentOffset = currentOffset.ToString();
+            classTimeZone = new TimeSpan(-8, 0, 0);
+            ViewBag.calculatedClassTimeZone = classTimeZone.ToString();
+            TimeSpan difference = classTimeZone - currentOffset;
+            ViewBag.vdifference = difference.ToString();
+
+            DateTime StartTime = DateTime.Parse("1:00:00 PM");
+            ViewBag.origStarttime = StartTime.ToString();
+
+            DateTime EndTime = StartTime;
+
+            EndTime = EndTime.Subtract(difference);
+            ViewBag.StartTimeAfterOffset = EndTime.ToString();
+
+            return View();
+        }
+
+    }
+}
