c90fd020533e61c0950625803eec95af7fac239d
dolivares_cp <SND\dolivares_cp>
2014-07-02 18:54:37 +0000

    filtered assignment types and re-ordered.

--- branches/arvixe/OSBLE.Web/Areas/AssignmentWizard/Controllers/HomeController.cs
+++ branches/arvixe/OSBLE.Web/Areas/AssignmentWizard/Controllers/HomeController.cs
@@ -127,31 +127,7 @@ namespace OSBLE.Areas.AssignmentWizard.Controllers
                 model = db.Assignments.Find((int)assignmentId);
             }
             return View(model);
-        }
-
-        [HttpGet]
-        public string GetCourseType()
-        {  
-            string courseType = "DEFAULT";            
-
-            //ActiveCourseUser.AbstractRoleID
-
-            switch (ActiveCourseUser.AbstractRoleID)
-            {
-                case (int)CourseRole.CourseRoles.Instructor:
-                    courseType = "Course";
-                    break;
-                case (int)CommunityRole.OSBLERoles.Leader:
-                    courseType = "Community";
-                    break;
-                case 12: case 13: case 14:
-                    courseType = "OTHER";
-                    break;
-                default:
-                    break;
-            }
-            return courseType;
-        }
+        }       
 
         #endregion
 
