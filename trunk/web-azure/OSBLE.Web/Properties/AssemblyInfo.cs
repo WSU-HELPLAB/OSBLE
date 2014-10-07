d6a8b878f40913fb8d559e60c8b2e491882aa21c
dolivares_cp <SND\dolivares_cp>
2014-10-07 05:46:39 +0000

    added note to file and folder upload informing user of special character stripping

--- trunk/web-azure/OSBLE.Web/Properties/AssemblyInfo.cs
+++ trunk/web-azure/OSBLE.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("14.9.23.36519")]
-[assembly: AssemblyFileVersion("14.9.23.36519")]
+[assembly: AssemblyVersion("14.10.6.36721")]
+[assembly: AssemblyFileVersion("14.10.6.36721")]
