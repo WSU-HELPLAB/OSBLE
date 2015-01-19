211610d9eba0c45bd41a5e4079b52267c7520ffe
dolivares_cp <SND\dolivares_cp>
2015-01-19 19:30:34 +0000

    account creation/roster bug fixed: whitelisted users will now be added to courses they are enrolled in as well as assignments that are already present.

--- trunk/web-azure/OSBLE.Web/Properties/AssemblyInfo.cs
+++ trunk/web-azure/OSBLE.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("14.11.17.37162")]
-[assembly: AssemblyFileVersion("14.11.17.37162")]
+[assembly: AssemblyVersion("15.1.12.37810")]
+[assembly: AssemblyFileVersion("15.1.12.37810")]
