5656df299c59b1446ae42cffa2083902893d9bfd
yeanwang99_cp <SND\yeanwang99_cp>
2015-06-01 12:19:00 +0000

    Undelete for now, but need to fix TFS issues ...

---
+++ trunk/web-azure/OSBLEPlus.Services.Tests/Analytics/Calendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSBLEPlus.Logic.DomainObjects;
+using OSBLEPlus.Logic.DomainObjects.Analytics;
+using OSBLEPlus.Services.Controllers;
+
+namespace OSBLEPlus.Services.Tests.Analytics
+{
+    [TestClass]
+    public class Calendar
+    {
+        [TestMethod]
+        public void CanGetDailyAggregations()
+        {
+            var results = new CalendarController().Get(
+                new CalendarAttributes
+                {
+                    AggregateFunctionId = AggregateFunction.Total,
+                    CourseId = 1,
+                    ReferenceDate = new DateTime(2014, 1, 1),
+                    SelectedMeasures = "ActiveStudents,LinesOfCodeWritten,TimeSpent,NumberOfCompilations,NumberOfErrorsPerCompilation,NumberOfNoDebugExecutions,NumberOfDebugExecutions,NumberOfBreakpointsSet,NumberOfRuntimeExceptions,NumberOfPosts,NumberOfReplies,TimeToFirstReply",
+                    SubjectUsers = null
+                });
+
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.GetType() == typeof(DailyAggregations));
+        }
+    }
+}
