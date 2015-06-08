efa829ad824f8fb92bb02c65977680aa86b904c3
bobma99_cp <SND\bobma99_cp>
2015-06-08 23:28:11 +0000

    Added an extension to DateTime that handles conversions between utc and course time, and applies this change to a few locations. Fixes back to back assignments from combining.

---
+++ OSBLE.Web/Utility/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OSBLE.Controllers;
+using OSBLE.Models;
+using OSBLE.Models.Courses;
+
+public static class DateTimeExtensions
+{
+    public static DateTime CourseToUTC(this DateTime date, int courseID)
+    {
+        OSBLEContext db = new OSBLEContext();
+        int offsetVal = ((Course) db.AbstractCourses.Find(courseID)).TimeZoneOffset;
+        CourseController cc = new CourseController();
+        TimeZoneInfo tzInfo = cc.getTimeZone(offsetVal);
+
+        return TimeZoneInfo.ConvertTimeToUtc(date, tzInfo);
+    }
+    public static DateTime UTCToCourse(this DateTime date, int courseID)
+    {
+        OSBLEContext db = new OSBLEContext();
+        int offsetVal = ((Course)db.AbstractCourses.Find(courseID)).TimeZoneOffset;
+        CourseController cc = new CourseController();
+        TimeZoneInfo tzInfo = cc.getTimeZone(offsetVal);
+
+        return TimeZoneInfo.ConvertTimeFromUtc(date, tzInfo);
+    }
+}
+
