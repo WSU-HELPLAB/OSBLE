5eeb0f9d387704cdffa7216f190247fc1061ccaf
yeanwang99_cp <SND\yeanwang99_cp>
2015-06-18 00:08:27 +0000

    Added nullable CourseID column in EventLogs table

--- OSBLEPlus/trunk/development/OSBLEPlus.Database/Schema/Tables/EventLogs.sql
+++ OSBLEPlus/trunk/development/OSBLEPlus.Database/Schema/Tables/EventLogs.sql
@@ -4,9 +4,11 @@
     [EventDate]	   DATETIME NOT NULL,
     [CreatedDate]  DATETIME NOT NULL,
     [SenderId]     INT      NOT NULL,
+    [CourseId]     INT,
     CONSTRAINT [PK_EventLogs_Id] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_EventLogs_EventTypes] FOREIGN KEY ([EventTypeId]) REFERENCES [dbo].[EventTypes] ([EventTypeId]),
-    CONSTRAINT [FK_EventLogs_UserProfiles] FOREIGN KEY ([SenderId]) REFERENCES [dbo].[UserProfiles] ([ID])
+    CONSTRAINT [FK_EventLogs_UserProfiles] FOREIGN KEY ([SenderId]) REFERENCES [dbo].[UserProfiles] ([ID]),
+    CONSTRAINT [FK_EventLogs_AbstractCourses] FOREIGN KEY ([CourseId]) REFERENCES [dbo].[AbstractCourses] ([ID])
 );
 GO
 ALTER TABLE [dbo].[EventLogs] ADD CONSTRAINT [DF_EventLogs] DEFAULT GETDATE() FOR [CreatedDate]
