e1d10652238950a100eee33006ccbd1fe321695d
dolivares_cp <SND\dolivares_cp>
2015-06-19 22:02:21 +0000

    Added random CourseId seed values, set up Analytics tab to allow for additional analytic options to be added.

---
+++ OSBLEPlus/trunk/development/OSBLE.Web/Areas/Analytics/Controllers/AnalyticsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+using OSBLE.Attributes;
+using OSBLE.Controllers;
+using OSBLEPlus.Logic.DomainObjects.Analytics;
+
+namespace OSBLE.Areas.Analytics.Controllers
+{
+    [OsbleAuthorize]
+    [IsAdmin]
+    public class AnalyticsController : OSBLEController
+    {
+        //
+        // GET: /Analytics/Calendar/
+        public ActionResult Index()
+        {
+            ViewBag.CurrentTab = "Analytics";
+            return View("Index");
+        }
+    }
+}
