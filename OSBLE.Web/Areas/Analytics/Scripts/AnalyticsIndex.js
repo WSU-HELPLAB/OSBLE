3c0d8c11ed8225946806801b5aaf27cd45349a0a
Daniel Olivares <d-olivares@users.noreply.github.com>
2015-06-20 01:07:48 +0000

    Added basic menu for choosing analytic features. work in progress.

---
+++ OSBLE.Web/Areas/Analytics/Scripts/AnalyticsIndex.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+    
+    
+
+});
+
+$(".menu_item").click(function () {
+    //debugger;
+    //remove id="current" from previous tab
+    $(".current").removeClass("current");
+    //add id="current" to current tab
+    $(this).addClass("current");
+
+});
