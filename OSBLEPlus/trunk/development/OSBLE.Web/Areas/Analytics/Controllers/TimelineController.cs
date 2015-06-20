a72d1d55a2f7cde4e5a5519bd908930ce7d76598
dolivares_cp <SND\dolivares_cp>
2015-06-20 01:07:48 +0000

    Added basic menu for choosing analytic features. work in progress.

--- OSBLEPlus/trunk/development/OSBLE.Web/Areas/Analytics/Controllers/TimelineController.cs
+++ OSBLEPlus/trunk/development/OSBLE.Web/Areas/Analytics/Controllers/TimelineController.cs
@@ -6,14 +6,14 @@ namespace OSBLE.Areas.Analytics.Controllers
 {
     [OsbleAuthorize]
     [IsAdmin]
-    public class CalendarController : OSBLEController
+    public class TimelineController : OSBLEController
     {
         //
         // GET: /Analytics/Calendar/
         public ActionResult Index()
         {
             //return View("Calendar", new CalendarAttributes { ReferenceDate = DateTime.Today });
-            return PartialView("_Calendar");
+            return PartialView("_Timeline");
         }
     }
 }
