37b9d8904bf824008602ed9251e8f1ba48e36c8a
dolivares_cp <SND\dolivares_cp>
2015-06-23 02:57:26 +0000

    Added menu structure for analytics.

--- OSBLEPlus/trunk/development/OSBLE.Web/Areas/Analytics/Scripts/AnalyticsIndex.js
+++ OSBLEPlus/trunk/development/OSBLE.Web/Areas/Analytics/Scripts/AnalyticsIndex.js
@@ -1,15 +1,32 @@
 ﻿
 $(document).ready(function () {
     
+    $(".menu_item").click(function () {
+        //debugger;
+        //remove id="current" from previous tab
+        $(".current").removeClass("current");
+        //add id="current" to current tab
+        $(this).addClass("current");
+
+        //$("#analytics_options").load("/Areas/Views/Analytics/_DefaultOptions.cshtml");
+        //$.ajax({
+        //        url: '/Analytics/Analytics/GetOptions',
+        //        contentType: 'application/html; charset=utf-8',
+        //        type: 'GET',
+        //        dataType: 'html'
+        //    })
+        //    .success(function(result) {
+        //        $("#analytics_options").html(result);
+        //    })
+        //    .error(function(xhr, status) {
+        //        alert(status);
+        //    });
+    });
+
+});
+
+
+
+function updateCurrentTab() {
     
-
-});
-
-$(".menu_item").click(function () {
-    //debugger;
-    //remove id="current" from previous tab
-    $(".current").removeClass("current");
-    //add id="current" to current tab
-    $(this).addClass("current");
-
-});
+}
