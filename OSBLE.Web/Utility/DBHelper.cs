3a655f2912bdf14f5d6ca1117c055e28d4aaf15a
Carson Tolleshaug <CarsonTolleshaug@users.noreply.github.com>
2015-06-24 22:24:42 +0000

    The new navigation bar is fully implemented and integrated into the site, which now uses bootstrap styles underneath the previous site.css styles.
    A new class called DBHelper has been added to help centralize dapper calls. include a "using OSBLE.Utility" directive to make use of it. 
    
    NOTE: If you're using IE, you will need to delete your browsing history before debugging again, because it keeps a cached copy of css and js files.

---
+++ OSBLE.Web/Utility/DBHelper.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using OSBLEPlus.Logic.Utility;
+using System.Data.SqlClient;
+using OSBLE.Models.Courses;
+using OSBLE.Models.DiscussionAssignment;
+
+namespace OSBLE.Utility
+{
+    /// <summary>
+    /// This is a class designed to make dapper database calls unified from a central location.
+    /// Add any static getter and/or setter for any chunk of data needed from the db here.
+    /// Try to follow the standard naming convention used here, as it makes it easier to find
+    /// these methods in the future, and prevents duplicate methods.
+    /// </summary>
+    public static class DBHelper
+    {
+        public static SqlConnection GetNewConnection()
+        {
+            return new SqlConnection(StringConstants.ConnectionString);
+        }
+
+        public static string GetCourseShortNameFromID(int courseID, SqlConnection connection = null)
+        {
+            string name = "";
+
+            // Set up our connection:
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { name = GetCourseShortNameFromID(courseID, sqlc); }
+            }
+            else
+            {
+                var result = connection.Query<dynamic>("SELECT Prefix, Number FROM AbstractCourses WHERE ID = @id", new { id = courseID }).SingleOrDefault();
+                if (result != null)
+                    name = result.Prefix + " " + result.Number;
+            }
+
+            return name;
+        }
+
+
+        public static IEnumerable<CourseUser> GetCoursesFromUserProfileID(int userProfileID, SqlConnection connection = null)
+        {
+            IEnumerable<CourseUser> courses = null;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { courses = GetCoursesFromUserProfileID(userProfileID, sqlc); }
+            }
+            else
+            {
+                IEnumerable<int> roleIDs = GetRoleIDsFromDiscriminator("CourseRole", connection);
+                courses = connection.Query<CourseUser>("SELECT * FROM CourseUsers WHERE UserProfileID = @uid AND AbstractRoleID IN @rids",
+                    new { uid = userProfileID, rids = roleIDs });
+            }
+
+            return courses;
+        }
+
+        public static IEnumerable<CourseUser> GetCommunitiesFromUserProfileID(int userProfileID, SqlConnection connection = null)
+        {
+            IEnumerable<CourseUser> communities = null;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { communities = GetCommunitiesFromUserProfileID(userProfileID, sqlc); }
+            }
+            else
+            {
+                IEnumerable<int> roleIDs = GetRoleIDsFromDiscriminator("CommunityRole",connection);
+                communities = connection.Query<CourseUser>("SELECT * FROM CourseUsers WHERE UserProfileID = @uid AND AbstractRoleID IN @rids",
+                    new { uid = userProfileID, rids = roleIDs });
+            }
+
+            return communities;
+        }
+
+
+        public static string GetCourseFullNameFromCourseUser(CourseUser cu, SqlConnection connection = null)
+        {
+            string name = "";
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { name = GetCourseFullNameFromCourseUser(cu, sqlc); }
+            }
+            else
+            {
+                var result = connection.Query<dynamic>("SELECT Name, Prefix, Number, Semester, Year, Inactive, Discriminator FROM AbstractCourses WHERE ID = @id",
+                    new { id = cu.AbstractCourseID }).SingleOrDefault();
+
+                if (result.Discriminator == "Course")
+                    name = string.Format("{0} {1} - {2}, {3}, {4}", result.Prefix, result.Number, result.Name, result.Semester, result.Year);
+                else
+                    name = result.Name; // communities are much simpler
+
+                // tack role on to the end
+                name += " (" + GetAbstractRoleNameFromID(cu.AbstractRoleID, connection) + ")";
+
+                if (result.Inactive)
+                    name += " [INACTIVE]";
+            }
+            
+            return name;
+        }
+
+        public static string GetAbstractRoleNameFromID(int ID, SqlConnection connection = null)
+        {
+            string name = "";
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { name = GetAbstractRoleNameFromID(ID, sqlc); }
+            }
+            else
+            {
+                name = connection.Query<string>("SELECT [Name] FROM AbstractRoles WHERE ID = @id", new { id = ID }).SingleOrDefault();
+            }
+
+            return name;
+        }
+
+        public static IEnumerable<int> GetRoleIDsFromDiscriminator(string discriminator, SqlConnection connection = null)
+        {
+            IEnumerable<int> ids = null;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { ids = GetRoleIDsFromDiscriminator(discriminator, sqlc); }
+            }
+            else
+            {
+                ids = connection.Query<int>("SELECT [ID] FROM AbstractRoles WHERE Discriminator = @disc", new { disc = discriminator });
+            }
+
+            return ids;
+        }
+
+        
+        
+        /*** Discussion Teams **********************************************************************************************/
+
+        public static int GetDiscussionTeamIDFromTeamID(int teamID, SqlConnection connection = null)
+        {
+            int dtID;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { dtID = GetDiscussionTeamIDFromTeamID(teamID, sqlc); }
+            }
+            else
+            {
+                dtID = connection.Query<int>(
+                        "SELECT Top 1 [ID] FROM DiscussionTeams WHERE TeamID = @id",
+                        new { id = teamID }).SingleOrDefault();
+            }
+
+            return dtID;
+        }
+
+        public static IEnumerable<DiscussionPost> GetDiscussionPosts(int courseUserID, int discussionTeamID, SqlConnection connection = null)
+        {
+            IEnumerable<DiscussionPost> dps = null;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { dps = GetDiscussionPosts(courseUserID, discussionTeamID, sqlc); }
+            }
+            else
+            {
+                dps = connection.Query<DiscussionPost>(
+                        "SELECT * FROM DiscussionPosts WHERE DiscussionTeamID = @did AND CourseUserID = @cid",
+                        new { did = discussionTeamID, cid = courseUserID });
+            }
+
+            return dps;
+        }
+
+        public static IEnumerable<DiscussionPost> GetInitialDiscussionPosts(int courseUserID, int assignmentID, SqlConnection connection = null)
+        {
+            IEnumerable<DiscussionPost> dps = null;
+
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { dps = GetInitialDiscussionPosts(courseUserID, assignmentID, sqlc); }
+            }
+            else
+            {
+                dps = connection.Query<DiscussionPost>(
+                    "SELECT * FROM DiscussionPosts WHERE CourseUserID = @cid AND AssignmentID = @aid AND ParentPostID IS NULL",
+                    new { cid = courseUserID, aid = assignmentID });
+            }
+
+            return dps;
+        }
+
+        public static void InsertDiscussionPosts(IEnumerable<DiscussionPost> posts, SqlConnection connection = null)
+        {
+            if (connection == null)
+            {
+                using (SqlConnection sqlc = GetNewConnection()) { InsertDiscussionPosts(posts, sqlc); }
+            }
+            else
+            {
+                connection.Execute(
+                        "INSERT DiscussionPosts (Posted, CourseUserID, Content, AssignmentID, DiscussionTeamID) VALUES (@Posted, @CourseUserID, @Content, @AssignmentID, @DiscussionTeamID)",
+                        posts);
+            }
+        }
+    }
+}
