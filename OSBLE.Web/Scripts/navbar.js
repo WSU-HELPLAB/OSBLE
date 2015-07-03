86a46885ad9a2af17f57b1868d19e6ea3091c1c8
Carson Tolleshaug <CarsonTolleshaug@users.noreply.github.com>
2015-07-03 21:49:01 +0000

    Navbar now employs a "More" dropdown in order to shrink to a reasonable size when the user has many tabs.

---
+++ OSBLE.Web/Scripts/navbar.js
@@ -0,0 +1,48 @@
+﻿
+// Makes tabs go under "more" dropdown to save space on the navbar
+function ResizeTabs() {
+    var w = $(window).width();
+
+    if ($(".navbar-toggle").css("display") != "none") {
+        if ($(".MoreTab").length > 0) {
+            $(".MoreTab").insertAfter($(".NavTab").last()).removeClass("MoreTab").addClass("NavTab");
+        }
+        $("#MoreDrop").css("display", "none");
+    }
+    else {
+        var tabWidthAffordance = 35 + $(".navbar-header").width() + $("#NavList").width() + $(".navbar-right").width();
+        while (tabWidthAffordance > w) {
+            // Shrink
+            var tab = $(".NavTab").last();
+
+            tabWidthAffordance -= tab.width();
+            tab.data("twidth", tab.width());
+
+            tab.prependTo($("#MoreList"));
+            tab.removeClass("NavTab").addClass("MoreTab");
+
+            $("#MoreDrop").css("display", "block");
+        }
+        if ($(".MoreTab").length > 0) {
+            var tab = $(".MoreTab").first();
+
+            while (tab.data("twidth") + tabWidthAffordance <= w) {
+                // We can fit the next tab
+                tabWidthAffordance += tab.data("twidth");
+                tab.insertAfter($(".NavTab").last());
+                tab.removeClass("MoreTab").addClass("NavTab");
+            }
+
+            if($(".MoreTab").length == 0)
+            {
+                $("#MoreDrop").css("display", "none");
+            }
+        }
+    }
+}
+
+$(document).ready(function () {            
+    $(window).resize(function () { ResizeTabs(); });
+    ResizeTabs();
+    ResizeTabs(); // For some reason, running this only once will still result in stacking. But running it twice seems to fix that.
+});
