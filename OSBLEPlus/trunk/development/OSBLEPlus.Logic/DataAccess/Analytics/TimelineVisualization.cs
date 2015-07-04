a704aef587185d2d0ed30840f0e70785fda0e5fb
bobma99_cp <SND\bobma99_cp>
2015-07-04 04:33:05 +0000

    Checked in missing project files, corrected namespaces

--- OSBLEPlus/trunk/development/OSBLEPlus.Logic/DataAccess/Analytics/TimelineVisualization.cs
+++ OSBLEPlus/trunk/development/OSBLEPlus.Logic/DataAccess/Analytics/TimelineVisualization.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
-using OSBIDE.Data.SQLDatabase;
 using OSBLEPlus.Logic.DomainObjects.Analytics;
 using OSBLEPlus.Logic.Utility;
 
