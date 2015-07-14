25c889d94d7b2d59b6dea4730ee3ee7d22ce722f
yeanwang99_cp <SND\yeanwang99_cp>
2015-07-14 06:40:00 +0000

    Add missing partials

---
+++ OSBLEPlus/trunk/development/OSBLEPlus.VSPlugin/OSBIDE.Plugins.VS2013/OSBIDE.Plugins.VS2013Package.ServiceClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+using OSBIDE.Library.ServiceClient;
+using OSBIDE.Library.ServiceClient.ServiceHelpers;
+
+namespace WashingtonStateUniversity.OSBIDE_Plugins_VS2013
+{
+    public sealed partial class OsbidePluginsVs2013Package
+    {
+        private void SetupServiceClient()
+        {
+            _eventHandler = new VsEventHandler(this, EventGenerator.GetInstance());
+            _client = ServiceClient.GetInstance(_eventHandler, _errorLogger);
+            _client.PropertyChanged += ServiceClientPropertyChanged;
+            _client.ReceivedNewSocialActivity += ServiceClientReceivedSocialUpdate;
+        }
+        void ServiceClientPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            UpdateServiceConnectionStatus();
+        }
+
+        void ServiceClientReceivedSocialUpdate(object sender, EventArgs e)
+        {
+            ToggleProfileImage(true);
+        }
+    }
+}
