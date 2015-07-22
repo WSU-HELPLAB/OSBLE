9a3a04967c207486fc77a1781426c5e660c330a7
AJArrott_cp <SND\AJArrott_cp>
2015-07-22 01:06:20 +0000

    AJ: Merged over my ActivityFeed javascript into a common file.

--- OSBLEPlus/trunk/development/OSBLE.Web/Scripts/ActivityFeed.js
+++ OSBLEPlus/trunk/development/OSBLE.Web/Scripts/ActivityFeed.js
@@ -148,6 +148,37 @@ function updateFeedItemViewModel(jsObject) {
     });
 }
 
+// global for updating text back to the correct text if canceled/changed when editing
+var text = '';
+
+function updateText(id) {
+    // get the div we're working with
+    var originalText = $("#content-comment-" + id);
+    var editForm = $("#edit-form-items-" + id);
+
+    // make sure we're not already editing
+    if (originalText.html() !== "Editing...") {
+        text = originalText.html();
+        originalText.html("Editing...");
+        editForm.show("blind");
+    }
+}
+
+function editText(id, bool) {
+    var editForm = $("#edit-form-items-" + id);
+    var originalText = $('#content-comment-' + id);
+    var textFromInput = $('#edit-form-textarea-' + id);
+
+    if (bool === true) {
+        originalText.html(textFromInput.val());
+    }
+    else {
+        originalText.html(text);
+    }
+
+    editForm.hide("blind");
+}
+
 /*
 //Periodically updates view models for feed items.  Useful for displaying an updated count
 //of comments for a given feed item.
