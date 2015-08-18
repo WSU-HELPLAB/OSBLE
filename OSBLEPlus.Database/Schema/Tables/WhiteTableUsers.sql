aa2e6ebe589b9c7e0b470d448aab64e244929e85
Daniel Olivares <d-olivares@users.noreply.github.com>
2015-08-18 19:13:35 +0000

    allow emails to be null in white table users

--- OSBLEPlus.Database/Schema/Tables/WhiteTableUsers.sql
+++ OSBLEPlus.Database/Schema/Tables/WhiteTableUsers.sql
@@ -5,7 +5,7 @@
     [Identification] NVARCHAR (MAX) NOT NULL,
     [Name1]          NVARCHAR (MAX) NULL,
     [Name2]          NVARCHAR (MAX) NULL,
-    [Email]          NVARCHAR (MAX) NOT NULL,
+    [Email]          NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_dbo.WhiteTableUsers] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_dbo.WhiteTableUsers_dbo.Schools_SchoolID] FOREIGN KEY ([SchoolID]) REFERENCES [dbo].[Schools] ([ID]) ON DELETE CASCADE
 );
