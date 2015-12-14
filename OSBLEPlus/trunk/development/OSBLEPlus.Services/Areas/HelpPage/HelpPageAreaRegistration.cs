861fffff7d1e2631660309e182e0af818bdebc69
AJArrott_cp <SND\AJArrott_cp>
2015-12-14 04:34:14 +0000

    AJ: Fixed OSBLE Help page it now links to the correct webpage.  Changed {servicesRoot}/Help to {servicesRoot}/APIHelp to help distinguish the difference between the osble help and the API help pages.

--- OSBLEPlus/trunk/development/OSBLEPlus.Services/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ OSBLEPlus/trunk/development/OSBLEPlus.Services/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -16,9 +16,9 @@ namespace OSBLEPlus.Services.Areas.HelpPage
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "HelpPage_Default",
-                "Help/{action}/{apiId}",
-                new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
+                "HelpPage_Services",
+                "APIHelp/{action}/{apiId}",
+                new { controller = "APIHelp", action = "Index", apiId = UrlParameter.Optional });
 
             HelpPageConfig.Register(GlobalConfiguration.Configuration);
         }
