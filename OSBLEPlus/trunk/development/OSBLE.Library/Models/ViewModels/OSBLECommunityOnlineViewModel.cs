81a54c628c8794eb627b43c7ac2cd3cac427d7fb
dolivares_cp <SND\dolivares_cp>
2016-02-18 05:07:07 +0000

    osblecommunity "who's online" now populates based on activity within set timeframe

---
+++ OSBLEPlus/trunk/development/OSBLE.Library/Models/ViewModels/OSBLECommunityOnlineViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSBLE.Models.ViewModels
+{
+    public class OSBLECommunityOnlineViewModel
+    {
+        public Dictionary <string, int> OnlineUser { get; set; }
+        
+        public OSBLECommunityOnlineViewModel()
+        {
+            OnlineUser = new Dictionary<string, int>();
+        }
+    }
+}
