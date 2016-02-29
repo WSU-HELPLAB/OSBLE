8b7ae2c893c1a2da0d3a5395439d7d8137a76c56
AJArrott_cp <SND\AJArrott_cp>
2016-02-29 19:10:35 +0000

    AJ: Added the ability to parse out the description label of enums to get nicer names in the view.  See EventType.cs and GetEnumDisplayName.cs for examples on how to use this.

---
+++ OSBLEPlus/trunk/development/OSBLEPlus.Logic/Utility/Lookups/GetEnumDisplayName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSBLEPlus.Logic.Utility.Lookups
+{
+    public class GetEnumDisplayName
+    {
+        /// <summary>
+        /// Displays Description Tag attribute of any enum
+        /// </summary>
+        /// <param name="en">An enumerable with description labels</param>
+        /// <returns></returns>
+        public static string GetDisplayName(Enum en)
+        {
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+            if (memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attrs.Length > 0)
+                    return ((DescriptionAttribute)attrs[0]).Description;
+            }
+
+            return en.ToString();
+        }   
+    }
+}
