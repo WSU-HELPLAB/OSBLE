2e8bbd9d4b0c363922c16b18503b1ea1965bffc4
nvelaborja_cp <SND\nvelaborja_cp>
2016-06-09 16:33:21 +0000

    Changed minimum required password length on signup page to 7 to match the Membership.MinRequiredPasswordLength property used in tooltips

--- OSBLEPlus/trunk/development/OSBLE.Library/Models/AccountModels.cs
+++ OSBLEPlus/trunk/development/OSBLE.Library/Models/AccountModels.cs
@@ -54,7 +54,7 @@ namespace OSBLE.Models
         public string ConfirmEmail { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 7)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
