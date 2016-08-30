83e66162153a73b29ca704c958e8b77b07223a28
Daniel Olivares <d-olivares@users.noreply.github.com>
2016-08-30 18:48:57 +0000

    VS Plugin: submissions now include a submission timestamp (in course time) as part of the filename. This change also allows submission history, so submissions downloaded from the assignment details will now include all resubmissions and no longer deletes the previously submitted files.

--- OSBLEPlus.Logic/DataAccess/Activities/Posts.cs
+++ OSBLEPlus.Logic/DataAccess/Activities/Posts.cs
@@ -52,10 +52,19 @@ namespace OSBLEPlus.Logic.DataAccess.Activities
                         new { assignmentId = submit.AssignmentId, userId = submit.SenderId },
                         commandType: CommandType.StoredProcedure).SingleOrDefault();
 
+                    int timezoneOffset = connection.Query<int>("SELECT ISNULL(TimeZoneOffset, -8) FROM AbstractCourses WHERE ID = @courseId ",
+                        new { courseId = submit.CourseId }).SingleOrDefault();
+                    
+                    TimeZoneInfo tzInfo = GetTimeZone(timezoneOffset);
+                    DateTime utcKind = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+                    DateTime submitTimeStamp = TimeZoneInfo.ConvertTimeFromUtc(utcKind, tzInfo);
+
                     try
                     {
+                        string submitTimestampInCourseTime = submitTimeStamp.ToString("MM-dd-yyyy-HH-mm-ss");
+                        
                         OSBLE.Models.FileSystem.Directories.GetAssignmentWithId(submit.CourseId ?? 1
-                            , submit.AssignmentId, teamId).AddFile(string.Format("{0}.zip", submit.Sender.FullName), zipStream);
+                            , submit.AssignmentId, teamId).AddFile(string.Format("{0}-{1}.zip", submit.Sender.FullName, submitTimestampInCourseTime), zipStream);                        
                     }
                     catch (ZipException)
                     {
@@ -85,6 +94,104 @@ namespace OSBLEPlus.Logic.DataAccess.Activities
             {
                 return -1;
             }
+        }
+
+        public static TimeZoneInfo GetTimeZone(int tzoffset)
+        {
+            string zone = "";
+            switch (tzoffset)
+            {
+                case 0:
+                    zone = "Greenwich Standard Time";
+                    break;
+                case 1:
+                    zone = "W. Europe Standard Time";
+                    break;
+                case 2:
+                    zone = "E. Europe Standard Time";
+                    break;
+                case 3:
+                    zone = "Russian Standard Time";
+                    break;
+                case 4:
+                    zone = "Arabian Standard Time";
+                    break;
+                case 5:
+                    zone = "West Asia Standard Time";
+                    break;
+                case 6:
+                    zone = "Central Asia Standard Time";
+                    break;
+                case 7:
+                    zone = "North Asia Standard Time";
+                    break;
+                case 8:
+                    zone = "Taipei Standard Time";
+                    break;
+                case 9:
+                    zone = "Tokyo Standard Time";
+                    break;
+                case 10:
+                    zone = "AUS Eastern Standard Time";
+                    break;
+                case 11:
+                    zone = "Central Pacific Standard Time";
+                    break;
+                case 12:
+                    zone = "New Zealand Standard Time";
+                    break;
+                case 13:
+                    zone = "Tonga Standard Time";
+                    break;
+                case -1:
+                    zone = "Cape Verde Standard Time";
+                    break;
+                case -2:
+                    zone = "Mid-Atlantic Standard Time";
+                    break;
+                case -3:
+                    zone = "E. South America Standard Time";
+                    break;
+                case -4:
+                    zone = "Atlantic Standard Time";
+                    break;
+                case -5:
+                    zone = "Eastern Standard Time";
+                    break;
+                case -6:
+                    zone = "Central Standard Time";
+                    break;
+                case -7:
+                    zone = "Mountain Standard Time";
+                    break;
+                case -8:
+                    zone = "Pacific Standard Time";
+                    break;
+                case -9:
+                    zone = "Alaskan Standard Time";
+                    break;
+                case -10:
+                    zone = "Hawaiian Standard Time";
+                    break;
+                case -11:
+                    zone = "Samoa Standard Time";
+                    break;
+                case -12:
+                    zone = "Dateline Standard Time";
+                    break;
+                default:
+                    zone = "";
+                    break;
+            }
+            TimeZoneInfo tz;
+            if (zone != "")
+                tz = TimeZoneInfo.FindSystemTimeZoneById(zone);
+            else
+            {
+                //going to assume utc
+                tz = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            }
+            return tz;
         }
     }
 }
