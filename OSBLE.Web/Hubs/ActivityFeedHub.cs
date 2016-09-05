6e02bcbc3ad6dd26e3b3bbf2c8357645d1179a19
Daniel Olivares <d-olivares@users.noreply.github.com>
2016-09-05 18:07:30 +0000

    Activity Feed: websockets notification - added additional checks to prevent injecting posts as different users

--- OSBLE.Web/Hubs/ActivityFeedHub.cs
+++ OSBLE.Web/Hubs/ActivityFeedHub.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using OSBLE.Utility;
+using Newtonsoft.Json.Linq;
+using OSBLEPlus.Logic.DomainObjects.ActivityFeeds;
+using OSBLE.Models.Users;
+using OSBLEPlus.Logic.Utility.Auth;
 
 namespace OSBLE.Hubs
 {
@@ -43,8 +47,45 @@ namespace OSBLE.Hubs
 
         public void NotifyNewPost(object post)
         {
+            //verify that the post is legit before pushing out to listeners
+            //will match the post information with the cookie authkey and the database event.
+            JObject feedPost = JObject.FromObject(post);
+            int eventLogId = (int)feedPost.SelectToken("EventId");
             int courseID = int.Parse(Context.QueryString["courseID"]);
-            Clients.Group(courseID.ToString()).addNewPost(courseID, post);
+            int senderId = (int)feedPost.SelectToken("SenderId");
+            string senderFullName = (string)feedPost.SelectToken("SenderName");
+
+            //cookie authkey            
+            string authKey = "";
+
+            try
+            {
+                authKey = Context.Request.Cookies["AuthKey"].Value.Split('=').Last();
+            }
+            catch (Exception)
+            {
+                return; //no authkey cookie or split error so it will not authenticate
+            }
+
+            Authentication auth = new Authentication();
+            bool validAuth = auth.IsValidKey(authKey);
+            
+            if (!validAuth)
+                return; //will not validate, just exit
+
+            UserProfile authUserProfile = auth.GetActiveUser(authKey);
+
+            //verify to this post and user data from the database
+            ActivityEvent verifyPost = DBHelper.GetActivityEvent(eventLogId);
+            UserProfile profile = DBHelper.GetUserProfile(verifyPost.SenderId);
+
+            if (validAuth && //their cookie authkey has to be valid
+                new[] { verifyPost.SenderId, senderId, authUserProfile.ID }.All(sid => sid == verifyPost.SenderId) && //the sender Id has to match
+                verifyPost.CourseId == courseID && //the course has to match
+                new[] { profile.FullName, senderFullName, authUserProfile.FullName }.All(pfn => pfn == profile.FullName)) //the user full name has to match
+            {
+                Clients.Group(courseID.ToString()).addNewPost(courseID, post);
+            }
         }
 
         public void NotifyEditPost(int postID, string newContent, string timeString)
