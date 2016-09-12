f575a7b87da83ba8df678038fc31335b0a92c78e
Daniel Olivares <d-olivares@users.noreply.github.com>
2016-09-12 17:52:59 +0000

    Increment version numbers to push a plugin update

--- OSBLE.Web/Properties/AssemblyInfo.cs
+++ OSBLE.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.9.12.29959")]
-[assembly: AssemblyFileVersion("13.9.12.29959")]
+[assembly: AssemblyVersion("16.9.12.40299")]
+[assembly: AssemblyFileVersion("16.9.12.40299")]
