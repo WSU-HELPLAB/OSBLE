e3b5ca6fc49afc8a2f6f8cb191df8aa7365b83e8
d-olivares <daniel.olivares@wsu.edu>
2017-08-10 05:01:40 +0000

    Removed extra lines in submiteventproperties table schema - updated xmldocument

--- OSBLEPlus.Database/Schema/Tables/SubmitEventProperties.sql
+++ OSBLEPlus.Database/Schema/Tables/SubmitEventProperties.sql
@@ -1,6 +1,4 @@
-﻿
-
-CREATE TABLE [dbo].[SubmitEventProperties](
+﻿CREATE TABLE [dbo].[SubmitEventProperties](
 	[EventLogId] [int] NOT NULL,
 	[IsWebpageSubmit] [bit] NOT NULL,
 	[IsPluginSubmit] [bit] NOT NULL,
@@ -11,5 +9,3 @@ CREATE TABLE [dbo].[SubmitEventProperties](
 	[IsPluginSubmit] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-
-GO
