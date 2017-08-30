a2ee36e7b4e521bd49fb7af6cc02677088f838cc
d-olivares <daniel.olivares@wsu.edu>
2017-08-30 19:20:22 +0000

    Fix for saving rubric drafts.

--- OSBLE.Web/Controllers/RubricController.cs
+++ OSBLE.Web/Controllers/RubricController.cs
@@ -102,7 +102,7 @@ namespace OSBLE.Controllers
                 //Even if the user has selected to save as draft,
                 //it should store the DatePublished to be displayed when the draft was last saved.
                 viewModel.Evaluation.IsPublished = false;
-                DateTime rubricPublished = (DateTime)viewModel.Evaluation.DatePublished;
+                DateTime rubricPublished = viewModel.Evaluation.DatePublished == null ? DateTime.UtcNow : (DateTime)viewModel.Evaluation.DatePublished;
                 viewModel.Evaluation.DatePublished = DateTime.UtcNow;
 
                 OSBLE.Utility.DBHelper.RemoveNotifications(viewModel.SelectedAssignment, teamId, ActiveCourseUser, rubricPublished);
